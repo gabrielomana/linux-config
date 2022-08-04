2a7c49a3c9c815e8f30868b7d679cfeeff402967
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2022-08-04 17:22:15 +0200

    fix

--- Ubuntu/language.sh
+++ Ubuntu/language.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get -y install language-pack-es-base hunspell-es gnome-user-docs wspanish language-pack-gnome-es
+sudo apt-get -y install language-pack-es-base hunspell-es gnome-user-docs gnome-user-docs-es wspanish language-pack-gnome-es
 sudo apt-get -y install task-spanish-desktop
 sudo dpkg-reconfigure locales
 export LANG=es_ES.UTF-8
