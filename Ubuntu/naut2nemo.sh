350abf5d987017a1f44e9f72acbe659ab19fcf4c
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2022-08-04 11:26:22 +0200

    fic

--- Ubuntu/naut2nemo.sh
+++ Ubuntu/naut2nemo.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#Repos MINT
-sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa main" >> /etc/apt/sources.list.d/mint_vanessa.list'
-sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa upstream" >> /etc/apt/sources.list.d/mint_vanessa.list'
-sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa backport" >> /etc/apt/sources.list.d/mint_vanessa.list'
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
-sudo apt reinstall libxapp1 -y
-sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/mint.gpg
-sudo apt update
-sudo apt-get install linuxmint-keyring -y
-sudo apt update 2>&1 1>/dev/null | sed -ne 's/.NO_PUBKEY //p' | while read key; do if ! [[ ${keys[]} =~ "$key" ]]; then sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys "$key"; keys+=("$key"); fi; done
-#******************
-
 ###Nautilis>Nemo
 sudo apt purge nautilus gnome-shell-extension-desktop-icons -y
 sudo apt install nemo -y
