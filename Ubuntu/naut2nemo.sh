3839df520a001ad669f1d8188adc49ceb54795d8
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2022-08-05 10:49:11 +0200

    fix

--- Ubuntu/naut2nemo.sh
+++ Ubuntu/naut2nemo.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
 ####OPCION 1
-sudo sh -c 'echo "deb http://packages.linuxmint.com/ uma main" >> /etc/apt/sources.list.d/mint.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6616109451BBBF2
-sudo apt reinstall libxapp1 -y
-sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/mint.gpg
-sudo apt update
-sudo apt-get install linuxmint-keyring -y
+sudo add-apt-repository ppa:ubuntucinnamonremix/all
+sudo apt-get update
+sudo apt install nemo --install-recommends -y
+sudo apt -y install python-nemo nemo-compare nemo-terminal nemo-fileroller cinnamon-l10n mint-translations --install-recommends
 
-sudo apt install mint-dev-tools -y
-sudo apt-get install libglib2.0-dev -y
-sudo mkdir -p /git/
-sudo mkdir -p /git/nemo-extensions/
-sudo git clone https://github.com/linuxmint/nemo-extensions /git/nemo-extensions/
-cd /git/nemo-extensions/
-sudo git pull origin master
-sudo ./build nemo-python nemo-terminal nemo-compare
-sudo dpkg -i python-nemo*.deb
-sudo apt install gir1.2-xapp-1.0 -y
-sudo dpkg -i nemo-terminal*.deb nemo-compare*.deb
-sudo rm *.deb -rf
-cd -
+#sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa main" >> /etc/apt/sources.list.d/mint_vanessa.list'
+#sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa upstream" >> /etc/apt/sources.list.d/mint_vanessa.list'
+#sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa backport" >> /etc/apt/sources.list.d/mint_vanessa.list'
+#sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
+#sudo apt update
+#sudo apt reinstall libxapp1 -y
+#sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/mint.gpg
+#sudo apt install linuxmint-keyring -y
+#sudo apt update 2>&1 1>/dev/null | sed -ne 's/.NO_PUBKEY //p' | while read key; do if ! [[ ${keys[]} =~ "$key" ]]; then sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys "$key"; keys+=("$key"); fi; done
+
+#sudo sh -c 'echo "Package: *\nPin: release o=linuxmint\nPin-Priority: 101\n\n" >> /etc/apt/preferences.d/mint.pref'
+#sudo apt update
 
 #sudo apt purge nautilus gnome-shell-extension-desktop-icons -y
 #sudo apt install nemo -y
