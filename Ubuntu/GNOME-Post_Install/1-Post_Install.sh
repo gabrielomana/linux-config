507423b17294c26bba660aee0924b86e13845c47
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2022-08-07 17:42:50 +0200

    fix

--- Ubuntu/GNOME-Post_Install/1-Post_Install.sh
+++ Ubuntu/GNOME-Post_Install/1-Post_Install.sh
@@ -68,19 +68,42 @@ echo "**************************************************************************
 sleep 7
 
 ###### INSTALL NEMO / REMOVE NAUTILIUS
-clear
-echo "INSTALL NEMO / REMOVE NAUTILIUS"
-sudo dnf -y remove nautilus.x86_64 gnome-shell-extension-desktop-icons
-sudo dnf -y autoremove
+###Nautilis>Nemo
 
-sudo dnf -y  install nemo nemo.x86_64 nemo-extensions.x86_64 nemo-preview.x86_64 nemo-fileroller.x86_64 nemo-python-devel.x86_64 nemo-terminal.noarch nemo-compare.noarch
+sudo echo "deb http://packages.linuxmint.com una main upstream import backport" > /etc/apt/sources.list.d/linux-mint.list  
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A6616109451BBBF2
+sudo apt reinstall libxapp1 -y
+sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/mint.gpg
+sudo apt install linuxmint-keyring -y
+sudo apt update 2>&1 1>/dev/null | sed -ne 's/.NO_PUBKEY //p' | while read key; do if ! [[ ${keys[]} =~ "$key" ]]; then sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys "$key"; keys+=("$key"); fi; done
+#sudo apt -y install python-nemo nemo-compare nemo-terminal nemo-fileroller cinnamon-l10n mint-translations --install-recommends
+
+sudo sh -c 'echo "Package: *\nPin: release o=Ubuntu\nPin-Priority: 501\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo sh -c 'echo "Package: *\nPin: release o=LP-PPA-ubuntustudio-ppa-backports\nPin-Priority: 501\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo sh -c 'echo "Package: *\nPin: release o=LLP-PPA-pipewire-debian-pipewire-upstream\nPin-Priority: 501\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo sh -c 'echo "Package: *\nPin: release o=LP-PPA-kisak-kisak-mesa\nPin-Priority: 501\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo sh -c 'echo "Package: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 501\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo sh -c 'echo "Package: *\nPin: release o=LP-PPA-graphics-drivers\nPin-Priority: 501\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo sh -c 'echo "Package: *\nPin: release o=linuxmint\nPin-Priority: 100\n\n" >> /etc/apt/preferences.d/priority.pref'
+sudo apt update
+clear
+sudo apt install nemo -y
+
+sudo apt purge nautilus gnome-shell-extension-desktop-icons -y
+
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons true
-echo -e "[Desktop Entry]\nType=Application\nName=Files\nComment=Start Nemo desktop at log in\nExec=nemo-desktop\nOnlyShowIn=GNOME;Unity;\nAutostartCondition=GSettings org.nemo.desktop show-desktop-icons\nX-GNOME-AutoRestart=true" | sudo tee /etc/xdg/autostart/nemo-autostart.desktop
-sudo cp /usr/share/applications/nemo.desktop ~/.local/share/applications/nemo.desktop
-sudo grep -v -s  "OnlyShowIn=X-Cinnamon;" ~/.local/share/applications/nemo.desktop > tmpfile && mv tmpfile ~/.local/share/applications/nemo.desktop
+gsettings set org.nemo.desktop use-desktop-grid true
+echo -e "[Desktop Entry]\nType=Application\nName=Files\nExec=nemo-desktop\nOnlyShowIn=GNOME;Unity;\nX-Ubuntu-Gettext-Domain=nemo" | sudo tee /etc/xdg/autostart/nemo-autostart.desktop
+sudo apt -y install python-nemo nemo-compare nemo-terminal nemo-fileroller cinnamon-l10n mint-translations --install-recommends
 
+sudo apt install chrome-gnome-shell gnome-tweaks gnome-shell-extensions gnome-software -y
+sudo apt-get update –fix-missing
+sudo apt-get install -f
+sudo apt-get autoremove -y
+sudo dpkg --configure -a
+sudo apt-get clean -y
 echo "*************************************************************************************"
 sleep 7
 
