116ca6ec2828edf6b6abefc2f8b2d8bdede2f616
Gabriel Omana <gabrielomana@gmail.com>
2022-08-25 11:27:21 +0200

    commit message

--- Ubuntu/language.sh
+++ Ubuntu/language.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-sudo apt-get -y install language-pack-es-base language-pack-es hunspell hunspell-es wspanish
+clear
+sudo apt clean
+sudo apt-get -y install language-pack-es
+sudo apt-get -y install language-pack-es-base
+sudo apt-get -y install language-support-es
+sudo apt-get -y install aspell-es
+sudo apt-get -y install myspell-es
+sudo apt-get -y install hunspell hunspell-es wspanish
 sudo apt-get -y install task-spanish-desktop
+sudo rm -r /var/lib/apt/lists/*
+echo -e "Acquire::Languages { \"es\"; \"none\"; };" | sudo tee /etc/apt/apt.conf.d/languages
+sudo rm /etc/apt/sources.list -rf
+sudo cp sources_es.list /etc/apt/sources.list
+
 sudo dpkg-reconfigure locales
 export LANG=es_ES.UTF-8
 clear
+sudo rm /var/lib/apt/lists/*Translation*
+sudo apt clean
+sudo apt update
+
 sudo apt purge libreoffice* -y
 sudo apt autoremove -y
 sudo apt clean
 reboot
+
