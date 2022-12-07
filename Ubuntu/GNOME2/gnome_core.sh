28ee95eafd1b57fe11c716479644ca09156435e7
gabrielomana <gabrielomana@gmail.com>
2022-12-07 16:09:03 +0100

    Update gnome_core.sh

--- Ubuntu/GNOME2/gnome_core.sh
+++ Ubuntu/GNOME2/gnome_core.sh
@@ -21,9 +21,14 @@ sudo apt install vanilla-gnome-desktop gnome-session gnome-terminal language-pac
 sudo apt install gedit evince file-roller -y
 
 clear
-sudo apt purge libreoffice* gnome-maps gnome-weather gnome-contactcs gnome-music gnome-photos eog gpac rhythmbox totem* postfix -y
+sudo apt purge libreoffice* gnome-maps gnome-weather gnome-contacts gnome-music gnome-photos eog gpac totem* -y
+sudo apt remove postfix -y && apt purge postfix -y
+sudo apt autoremove -y
+sudo dpkg-reconfigure postfix
 
 sudo apt-get autoremove -y
+
+#REMOVE SNAP/ADD FLATPACK
 
 #remove snap service (existing sanp applications must be uninstalled first)
 sudo apt autoremove --purge snapd -y
@@ -33,6 +38,19 @@ sudo apt purge snapd
 sudo apt-mark hold snapd
 echo -e "Package: snapd\nPin: release a=*\nPin-Priority: -10" | sudo tee /etc/apt/preferences.d/nosnap.pref
 sudo apt update -y
+
+#limpiar y arreglar paquetes rotos
+sudo apt-get update –fix-missing
+sudo apt-get install -f
+sudo apt-get clean -y
+sudo apt-get autoremove -y
+sudo dpkg --configure -a
+
+#install flatpak service
+sudo apt install flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+clear
+
 
 #sudo apt-get -y install language-pack-es-base
 clear
@@ -52,16 +70,37 @@ sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo add-apt-repository multiverse -y
 sudo add-apt-repository ppa:mozillateam/ppa -y
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
+sudo add-apt-repository ppa:appimagelauncher-team/stable -y
 
 sudo apt install -t 'o=LP-PPA-mozillateam' firefox -y
 echo -e "Package: firefox*\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 501" | sudo tee /etc/apt/preferences.d/mozillateamppa.pref
+
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+echo -e "deb https://ppa.launchpadcontent.net/webupd8team/y-ppa-manager/ubuntu/ impish main\n# deb-src https://ppa.launchpadcontent.net/webupd8team/y-ppa-manager/ubuntu/ impish main" | sudo tee /etc/apt/sources.list.d/webupd8team-ubuntu-y-ppa-manager-jammy.list
+sudo wget --no-check-certificate -qO - https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=index&search=0x7B2C3B0889BF5709A105D03AC2518248EEA14886 | gpg --dearmor | sudo tee /usr/share/keyrings/webupd8team.gpg
+
+#Repos MINT
+
+sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa main" >> /etc/apt/sources.list.d/mint_vanessa.list'
+sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa upstream" >> /etc/apt/sources.list.d/mint_vanessa.list'
+sudo sh -c 'echo "deb http://packages.linuxmint.com/ vanessa backport" >> /etc/apt/sources.list.d/mint_vanessa.list'
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
+sudo apt update
+sudo apt reinstall libxapp1 -y
+sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/mint.gpg
+sudo apt install linuxmint-keyring -y
+sudo apt update 2>&1 1>/dev/null | sed -ne 's/.NO_PUBKEY //p' | while read key; do if ! [[ ${keys[]} =~ "$key" ]]; then sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys "$key"; keys+=("$key"); fi; done
+
+sudo sh -c 'echo "Package: *\nPin: release o=linuxmint\nPin-Priority: 101\n\n" >> /etc/apt/preferences.d/mint.pref'
+
 sudo apt update -y
 
 # CORE APPS
 clear
 echo -e "CORE\n"
 sudo apt install -y \
-build-essential software-properties-gtk gcc make perl g++ \
+build-essential software-properties-gtk gcc make perl g++ npm \
 wget curl git gdebi \
 dconf dconf-editor cabextract xorg-x11-font-utils fontconfig cmake anacron \
 software-properties-common ca-certificates gnupg2 ubuntu-keyring apt-transport-https \
@@ -71,7 +110,7 @@ ubuntu-restricted-extras \
 gstreamer1.0-libav ffmpeg x264 x265 h264enc mencoder mplayer \
 cabextract \
 samba \
-screen bleachbit \
+screen bleachbit y-ppa-manager\
 util-linux* apt-utils bash-completion openssl finger dos2unix nano sed numlockx
 
 #SYSTEM
@@ -80,9 +119,9 @@ echo -e "SYSTEM\n"
 sudo apt install -y \
 v4l2loopback-utils \
 neofetch \
-cups-pdf \
+printer-driver-cups-pdf \
 grub-customizer \
-tesseract tesseract-devel tesseract-langpack-cat tesseract-langpack-eng tesseract-langpack-spa gimagereader-qt \
+tesseract-ocr-spa gImageReader* \
 policycoreutils-gui firewall-config
 
 sudo npm install -g hblock
@@ -98,7 +137,8 @@ cheese \
 timeshift \
 flameshot \
 tilix \
-https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
+appimagelauncher \
+webapp-manager
 
 sudo gsettings set org.gnome.desktop.default-applications.terminal exec 'tilix'
 
@@ -108,7 +148,6 @@ echo -e "MULTIMEDIA\n"
 sudo apt install -y \
 smplayer \
 audacity \
-deadbeef deadbeef-mpris2-plugin deadbeef-plugins \
 shotwell
 
 #OFIMATICA  ******************************************#
@@ -116,5 +155,28 @@ clear
 echo -e "OFIMATICA\n"
 sudo apt install -y pdfarranger evince 
 
+#NAUTILUS > NEMO
+clear
+echo -e "NAUTILUS > NEMO\n"
+sudo apt -y install python-nemo nemo-compare nemo-terminal nemo-fileroller cinnamon-l10n mint-translations --install-recommends
+
+sudo apt purge nautilus gnome-shell-extension-desktop-icons -y
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+gsettings set org.gnome.desktop.background show-desktop-icons false
+gsettings set org.nemo.desktop show-desktop-icons true
+gsettings set org.nemo.desktop use-desktop-grid true
+echo -e "[Desktop Entry]\nType=Application\nName=Files\nExec=nemo-desktop\nOnlyShowIn=GNOME;Unity;\nX-Ubuntu-Gettext-Domain=nemo" | sudo tee /etc/xdg/autostart/nemo-autostart.desktop
+
+sudo apt install chrome-gnome-shell gnome-tweaks gnome-shell-extensions gnome-software -y
+sudo apt-get update –fix-missing
+sudo apt-get install -f
+sudo apt-get clean -y
+sudo apt-get autoremove -y
+sudo dpkg --configure -a
+
+#FULL UPDATE
+clear
+echo -e "FULL UPDATE\n"
 sudo apt clean -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y
 reboot
