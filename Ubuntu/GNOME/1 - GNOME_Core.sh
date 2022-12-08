5b95fc50a6ac62d4a3a7b805e31f676c1432435a
gabrielomana <gabrielomana@gmail.com>
2022-12-08 15:15:35 +0100

    Update 1 - GNOME_Core.sh

--- Ubuntu/GNOME/1 - GNOME_Core.sh
+++ Ubuntu/GNOME/1 - GNOME_Core.sh
@@ -13,28 +13,28 @@ echo -e "es_ES.UTF-8 UTF-8\nen_US.UTF-8 UTF-8" | sudo tee /var/lib/locales/suppo
 sudo dpkg-reconfigure locales
 
 
-clear
-# REMOVE SNAP/ADD FLATPACK
-echo "REMOVE SNAP/ADD FLATPACK"
-#remove snap service (existing sanp applications must be uninstalled first)
-sudo apt autoremove --purge snapd -y
-sudo rm -rf ~/snap
-sudo rm -rf /var/cache/snapd
-sudo apt purge snapd
-sudo apt-mark hold snapd
-echo -e "Package: snapd\nPin: release a=*\nPin-Priority: -10" | sudo tee /etc/apt/preferences.d/nosnap.pref
-sudo apt update -y
+# clear
+# # REMOVE SNAP/ADD FLATPACK
+# echo "REMOVE SNAP/ADD FLATPACK"
+# #remove snap service (existing sanp applications must be uninstalled first)
+# sudo apt autoremove --purge snapd -y
+# sudo rm -rf ~/snap
+# sudo rm -rf /var/cache/snapd
+# sudo apt purge snapd
+# sudo apt-mark hold snapd
+# echo -e "Package: snapd\nPin: release a=*\nPin-Priority: -10" | sudo tee /etc/apt/preferences.d/nosnap.pref
+# sudo apt update -y
 
-#limpiar y arreglar paquetes rotos
-sudo apt-get update –fix-missing
-sudo apt-get install -f
-sudo apt-get clean -y
-sudo apt-get autoremove -y
-sudo dpkg --configure -a
+# #limpiar y arreglar paquetes rotos
+# sudo apt-get update –fix-missing
+# sudo apt-get install -f
+# sudo apt-get clean -y
+# sudo apt-get autoremove -y
+# sudo dpkg --configure -a
 
-#install flatpak service
-sudo apt install flatpak -y
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# #install flatpak service
+# sudo apt install flatpak -y
+# sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 clear
 # GNOME
