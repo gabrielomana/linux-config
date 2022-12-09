856b6f0e92c9b3a03f837366a5af251034ae31ec
gabrielomana <gabrielomana@gmail.com>
2022-12-09 14:30:38 +0100

    Update 1 - GNOME_Core.sh

--- Ubuntu/GNOME/1 - GNOME_Core.sh
+++ Ubuntu/GNOME/1 - GNOME_Core.sh
@@ -1,17 +1,25 @@
 !/bin/bash
-# clear
-# # LANGUAGE
-# echo "LANGUAGE"
-# sudo apt clean
-# sudo apt-get -y install language-pack-es
-# sudo apt-get -y install language-pack-es-base
-# # cd /usr/share/locales/
-# # sudo ./install-language-pack es_ES
-# # echo -e "LANG=\"es_ES.UTF-8\"\nLC_ALL=\"es_ES.UTF-8\"\nLANGUAGE=\"es_ES\"" | sudo tee /etc/environment
-# # echo -e "LANG=\"es_ES.UTF-8\"\nLC_ALL=\"es_ES.UTF-8\"\nLANGUAGE=\"es_ES\"" | sudo tee /etc/default/locale
-# # echo -e "es_ES.UTF-8 UTF-8\nen_US.UTF-8 UTF-8" | sudo tee /var/lib/locales/supported.d/local
-# sudo dpkg-reconfigure locales
 
+clear
+#GNOME Vanilla Minimal
+echo "GNOME Vanilla Minimal"
+sudo apt install -y vanilla-gnome-desktop gnome-session xinit gnome-terminal gedit lightdm language-pack-gnome-es language-pack-gnome-es-base gnome-user-docs gnome-user-docs-es plymouth-theme-ubuntu-logo
+
+# clear
+# CLEAN GNOME
+echo "CLEAN GNOME"
+sudo apt purge libreoffice* gnome-maps gnome-weather gnome-contacts gnome-music gnome-photos eog gpac totem* -y
+sudo apt remove postfix -y && apt purge postfix -y
+sudo apt autoremove -y
+sudo dpkg-reconfigure postfix
+sudo apt-get autoremove -y
+sudo apt-get update –-fix-missing
+sudo apt-get install -f
+
+sudo systemctl set-default graphical.target
+
+#sudo systemctl enable gdm3
+#sudo systemctl enable lightdm
 
 # # clear
 # # # REMOVE SNAP/ADD FLATPACK
@@ -35,35 +43,6 @@
 # # #install flatpak service
 # # sudo apt install flatpak -y
 # # sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# clear
-# # GNOME
-# echo "GNOME"
-# #Ubunutu Minimal
-# #sudo apt install ubuntu-desktop-minimal language-pack-gnome-es language-pack-gnome-es-base gnome-user-docs gnome-user-docs-es plymouth-theme-ubuntu-logo  -y
-
-# #GNOME Vanilla Minimal
-
-sudo apt install -y vanilla-gnome-desktop gnome-session xinit gnome-terminal gedit lightdm language-pack-gnome-es language-pack-gnome-es-base gnome-user-docs gnome-user-docs-es plymouth-theme-ubuntu-logo
-
-# # sudo apt install -y gedit evince file-roller
-
-# clear
-# # CLEAN GNOME
-# echo "CLEAN GNOME"
-# # sudo apt purge libreoffice* gnome-maps gnome-weather gnome-contacts gnome-music gnome-photos eog gpac totem* -y
-# # sudo apt remove postfix -y && apt purge postfix -y
-# # sudo apt autoremove -y
-# # sudo dpkg-reconfigure postfix
-
-# # sudo apt-get autoremove -y
-
-# #sudo apt-get -y install language-pack-es-base
-# #sudo dpkg-reconfigure locales
-
-sudo systemctl set-default graphical.target
-#sudo systemctl enable gdm3
-#sudo systemctl enable lightdm
 
 
 # clear
