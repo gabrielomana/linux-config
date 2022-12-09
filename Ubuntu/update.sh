148ff2dca992ed88bf4c7e05184baa558665a998
gabrielomana <gabrielomana@gmail.com>
2022-12-09 13:35:33 +0100

    Update update.sh

--- Ubuntu/update.sh
+++ Ubuntu/update.sh
@@ -14,8 +14,8 @@ echo -e "LANG=\"es_ES.UTF-8\"\nLC_ALL=\"es_ES.UTF-8\"\nLANGUAGE=\"es_ES\"" | sud
 echo -e "es_ES.UTF-8 UTF-8\nen_US.UTF-8 UTF-8" | sudo tee /var/lib/locales/supported.d/local
 sudo dpkg-reconfigure locales
 sudo apt install -y dos2unix
-sudo chmod 777 linux-cnfig/Ubuntu/Gnome/*sh
-sudo chmod 777 linux-cnfig/Ubuntu/KDE_PLASMA/*sh
-dos2unix linux-cnfig/Ubuntu/Gnome/*sh
-dos2unix linux-cnfig/Ubuntu/KDE_PLASMA/*sh
+sudo chmod 777 linux-cnfig/Ubuntu/Gnome/*.sh
+sudo chmod 777 linux-cnfig/Ubuntu/KDE_PLASMA/*.sh
+dos2unix linux-cnfig/Ubuntu/Gnome/*.sh
+dos2unix linux-cnfig/Ubuntu/KDE_PLASMA/*.sh
 
