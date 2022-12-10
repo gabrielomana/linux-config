92eb89255d4d77a9f9c6db4fe54b9e0f14145d05
gabrielomana <gabrielomana@gmail.com>
2022-12-10 17:02:56 +0100

    Fix
    Fix

--- Ubuntu/GNOME/snap.sh
+++ Ubuntu/GNOME/snap.sh
@@ -1,22 +1,30 @@
+#!/bin/bash
 clear
-# REMOVE SNAP/ADD FLATPACK
-echo "REMOVE SNAP/ADD FLATPACK"
-#remove snap service (existing sanp applications must be uninstalled first)
-sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
-sudo rm -rf ~/snap
-sudo rm -rf /var/cache/snapd
-sudo apt purge snapd
+# remove --purge SNAP/ADD FLATPACK
+sudo systemctl disable snapd.service
+sudo systemctl disable snapd.socket
+sudo systemctl disable snapd.seeded.service
+sudo snap remove --purge firefox
+sudo snap remove --purge snap-store
+sudo snap remove --purge bare
+sudo snap remove --purge core20
+sudo snap remove --purge gnome-3-38-2004
+sudo snap remove --purge gtk-common-themes
+sudo snap remove --purge lxd
+sudo apt remove --autoremove snapd
+sudo rm -rf /var/cache/snapd/
+rm -rf ~/snap
 sudo apt-mark hold snapd
 echo -e "Package: snapd\nPin: release a=*\nPin-Priority: -10" | sudo tee /etc/apt/preferences.d/nosnap.pref
-sudo apt update -y
-
-#limpiar y arreglar paquetes rotos
-sudo apt-get update ?-fix-missing
+sudo apt-get autoremove --purge -y
+sudo apt install --install-suggests gnome-software
+sudo apt-get update --fix-missing
 sudo apt-get install -f
-sudo apt-get clean -y
-sudo apt-get autoremove -y
-sudo dpkg --configure -a
+
 
 #install flatpak service
 sudo apt install flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+
