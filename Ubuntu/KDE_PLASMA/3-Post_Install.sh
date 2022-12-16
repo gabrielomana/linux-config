1dc1b15fa3e119b5c2a0a8fbe5b11014ffd8aadf
gabrielomana <gabrielomana@gmail.com>
2022-12-16 12:28:39 +0100

    Update 3-Post_Install.sh

--- Ubuntu/KDE_PLASMA/3-Post_Install.sh
+++ Ubuntu/KDE_PLASMA/3-Post_Install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee ~/.zshrc
+echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee /root/.zshrc
 
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/ohmyzsh/ohmyzsh/ /tmp/ohmyzsh/
 ZSH= sh /tmp/ohmyzsh/tools/install.sh
 rm -rf /tmp/ohmyzsh/
 source ~/.zshrc 
-
-echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee ~/.zshrc
-echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee /root/.zshrc
