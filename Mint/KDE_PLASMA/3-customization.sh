5ab35a29fe3576aa9dfd74b82611b4cbf04b41f8
gabrielomana <gabrielomana@gmail.com>
2023-02-09 00:03:50 +0100

    Fix
    Fix

---
+++ Mint/KDE_PLASMA/3-customization.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+cd style/
+./install.sh
--- Mint/KDE_PLASMA/Style/install.sh
+++ Mint/KDE_PLASMA/Style/install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-if ! [ $(id -u) = 0 ]; then echo "Run as root" exit 1 fi
 
-mkdir /usr/share/themes/myPlasmaTheme-1.0 
-cp custom.htm /usr/share/themes/myPlasmaTheme-1.0/
-cp -rf CUSTOMIZACION_archivos/ /usr/share/themes/myPlasmaTheme-1.0/CUSTOMIZACION_archivos
-cp Pop_Os-Icons.tar.gz /usr/share/themes/myPlasmaTheme-1.0/
+if ! [ $(id -u) = 0 ]; then
+echo "Run as root"
+exit 1
+fi
+
+mkdir /usr/share/themes/myPlasmaTheme-1.0
+sudo cp custom.htm /usr/share/themes/myPlasmaTheme-1.0/
+sudo cp -rf CUSTOMIZACION_archivos/ /usr/share/themes/myPlasmaTheme-1.0/CUSTOMIZACION_archivos
+sudo cp Pop_Os-Icons.tar.gz /usr/share/themes/myPlasmaTheme-1.0/
 
 #Lightly
 cd /usr/share/themes/myPlasmaTheme-1.0
 
-apt install -y cmake build-essential libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev extra-cmake-modules libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev libkf5iconthemes-dev gettext qt3d5-dev
+nala install cmake build-essential libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev extra-cmake-modules libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev libkf5iconthemes-dev gettext qt3d5-dev -y
 
 git clone --single-branch --depth=1 https://github.com/Luwx/Lightly.git
 cd Lightly && mkdir build && cd build
@@ -20,7 +24,7 @@ make install
 #LightlyShaders
 cd /usr/share/themes/myPlasmaTheme-1.0
 
-apt install -y git cmake g++ gettext extra-cmake-modules qttools5-dev libqt5x11extras5-dev libkf5configwidgets-dev libkf5crash-dev libkf5globalaccel-dev libkf5kio-dev libkf5notifications-dev kinit-dev kwin-dev 
+nala install git cmake g++ gettext extra-cmake-modules qttools5-dev libqt5x11extras5-dev libkf5configwidgets-dev libkf5crash-dev libkf5globalaccel-dev libkf5kio-dev libkf5notifications-dev kinit-dev kwin-dev -y
 
 git clone https://github.com/Luwx/LightlyShaders
 cd LightlyShaders; mkdir qt5build; cd qt5build; cmake ../ -DCMAKE_INSTALL_PREFIX=/usr -DQT5BUILD=ON && make && make install && (kwin_x11 --replace &)
@@ -37,15 +41,19 @@ add-apt-repository ppa:papirus/papirus -y
 ##Fix deprecated Key MINT issue
 mv /etc/apt/trusted.gpg /etc/apt/papirus.gpg
 ln -s /etc/apt/papirus.gpg /etc/apt/trusted.gpg.d/papirus.gpg
-apt update && apt install -y papirus-icon-theme
+nala update && nala install papirus-icon-theme -y
 
 #ARC THEME &&  ARK-KDE  THEME
-sudo apt-get install -y --install-recommends arc-theme arc-kde
+sudo nala install arc-theme arc-kde -y --install-recommends
 
 #POP_OS ICONS
 cd /usr/share/themes/myPlasmaTheme-1.0
 tar -xf Pop_Os-Icons.tar.gz -C /usr/share/icons/
 
 cd /usr/share/themes/myPlasmaTheme-1.0
-firefox custom.htm
+
+u="$(logname)"
+runuser -l  $u -c 'firefox custom.htm'
+
+
 
