599df212ab99aaa6fc32a8d9e2659e4f323d0db0
gabrielomana <gabrielomana@gmail.com>
2023-02-08 17:28:29 +0100

    Fix
    Fix

--- Mint/KDE_PLASMA/1_install.sh
+++ Mint/KDE_PLASMA/1_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#sudo apt install nala -y
-#sudo nala fetch --auto --fetches 5 -y
 
-clear
+if command -v nala &> /dev/null; then
+    sudo nala fetch --auto --fetches 5 -y
+else
+    if sudo apt install nala -y ; then
+        sudo nala fetch --auto --fetches 5 -y
+    fi
+fi
+
 dir="$(pwd)"
 
 codecs="${dir}/sources/lists/codecs.list"
@@ -17,80 +22,69 @@ kde_bloatware="${dir}/sources/lists/kde_bloatware.list"
 
 . "${dir}"/sources/functions/functions
 
+
 ####################### UNINSTALL XFCE ###############################
 clear
-echo "UNINSTALL XFCE: "
+echo "UNINSTALL XFCE"
 sleep 3
-#uninstall_xfce
-# # ######################## REPOSITORIES ###############################
- clear
- echo "ADD REPOSITORIES"
- sleep 3
- #add_repos
-# ######################## KDE PLASMA ###############################
- clear
- echo "INSTALL KDE PLASMA: "
- sleep 3
- install_kde
-# ######################## CORE APPS ###############################
+uninstall_xfce
+########################## REPOSITORIES ###############################
+clear
+echo "ADD REPOSITORIES"
+sleep 3
+add_repos
+######################### KDE PLASMA ###############################
+clear
+echo "INSTALL KDE PLASMA: "
+sleep 3
+install_kde
+######################### CORE APPS ###############################
 clear
 echo "INSTALL SYSTEM CORE APPS: "
 sleep 3
 install_core_apps
 
-# ######################## MULTIMEDIA ###############################
+######################### MULTIMEDIA ###############################
 clear
-echo -e "INSTALL MULTIMEDIA APPS: \n"
+echo "INSTALL MULTIMEDIA APPS: "
 sleep 3
 install_multimedia
 
 
-# # ##############################################################################################
-# # ########################################  EXTRA APPS #########################################
-# # ##############################################################################################
+########################################## EXTRA APPS #########################################
 clear
-a=0
-j=0
-    while [ $a -lt 1  ]
-    do
-        read -p "EXTRA APPS: Do you wish to install Extra Apps? " yn
-        case $yn in
-            [Yy]* ) a=1;sleep 3;check_installed "${exta_apps}";clear;;
-            [Nn]* ) a=1;echo "OK\n";clear;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+echo "EXTRA APPS: "
+install_extra_apps
+
+#########################################_END_ #################################################
 
 
-# # #############################################################################################
-# # #######################################_END_#################################################
-# # #############################################################################################
-# #
-# # ######## CARGO & TOPGRADE #####################################
+########## CARGO & TOPGRADE #####################################
 clear
-echo -e "CARGO & TOPGRADE\n"
+echo "CARGO & TOPGRADE"
 sleep 3
 cargo install cargo-update
 cargo install topgrade
-echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee ~/.bashrc
-echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee /root/.bashrc
+echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee -a ~/.bashrc
+echo -e "export PATH=$HOME/.cargo/bin:/usr/local/bin:$PATH" | sudo tee -a /root/.bashrc
 
 
-# # ######## KONSOLE #############################################
+########## KONSOLE #############################################
 clear
-echo -e "KONSOLE & DOTFILES\n"
+echo "KONSOLE & DOTFILES"
 sleep 3
 sudo cp style/colors/* /usr/share/konsole/ -rf
 cp -r files/konsole.profile ~/.local/share/konsole/konsole.profile
 cp -r files/neofetch.conf ~/.config/neofetch/config.conf
 cp -r files/topgrade.toml ~/.config/topgrade.toml
 
-# # ######## FULL UPDATE ##########################################
+########## FULL UPDATE ##########################################
 clear
-echo -e "FULL UPDATE\n"
+echo "FULL UPDATE"
 sudo nala clean
 sleep 3
 sudo nala update; sudo nala upgrade -y; sudo nala install -f; sudo dpkg --configure -a; sudo nala autoremove; sudo apt --fix-broken install
+sudo apt full-upgrade -y
 sudo aptitude safe-upgrade -y
 sudo systemctl disable casper-md5check.service
 reboot
