d2e4b46a05899e861b990a81ad39a1f3549df4b7
gabrielomana <gabrielomana@gmail.com>
2023-02-13 09:37:03 +0100

    Fix
    Fix

--- Mint/KDE_PLASMA/2-post-install.sh
+++ Mint/KDE_PLASMA/2-post-install.sh
@@ -23,20 +23,6 @@ clear
 install_extra_apps
 
 
-########## ZSH+OHMYZSH+STARSHIP #############################################
-clear
-a=0
-j=0
-while [ $a -lt 1 ]
-do
-    read -p "Do you wish to install ZSH+OHMYZSH+STARSHIP? " yn
-    case $yn in
-        [Yy]* ) a=1;install_ZSH;clear;;
-        [Nn]* ) a=1;echo "OK";clear;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
 ########## CLEAN & FINAL STEPS #############################################
 clear
 echo "CLEAN & FINAL STEPS"
@@ -52,13 +38,17 @@ sudo cp files/grub /etc/default/grub
 sudo update-grub
 sudo mainline-gtk
 clear
-key=""
-read -r -s -n 1 -t 5 -p "Press any key to abort reboot in the next 5 seconds." key
-echo
-if [ "$key" = "" ]     # No Keypress detected, phone home.
-     then reboot
-     else
-        echo "Reboot to apply all changes"
-
-fi
+########## ZSH+OHMYZSH+STARSHIP #############################################
+clear
+a=0
+j=0
+while [ $a -lt 1 ]
+do
+    read -p "Do you wish to install ZSH+OHMYZSH+STARSHIP? " yn
+    case $yn in
+        [Yy]* ) a=1;install_ZSH;clear;;
+        [Nn]* ) a=1;echo "OK";clear;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
