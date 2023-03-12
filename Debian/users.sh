7d2f0d6db54acae3e44feee1897a8916f1ca40e9
gabrielomana <gabrielomana@gmail.com>
2023-03-12 21:27:55 +0100

    Fix
    Fix

--- Debian/users.sh
+++ Debian/users.sh
@@ -4,6 +4,9 @@ dir="$(pwd)"
 apt install curl wget apt-transport-https dirmngr apt-xapian-index software-properties-common ca-certificates gnupg dialog netselect-apt tree bash-completion util-linux build-essential dkms apt-transport-https bash-completion console-setup curl debian-reference-es linux-base lsb-release make man-db manpages memtest86+ gnupg linux-headers-$(uname -r) comm dos2unix systemd-sysv usbutils unrar-free zip rsync p7zip net-tools screen sudo -yy
 
 update-apt-xapian-index -vf
+cp ${dir}/dotfiles/2-sources.list /etc/apt/sources.list -rf
+apt update
+sleep 10
 
 users_local=$(cut -d: -f1 /etc/passwd)
 user_sys="root daemon bin sys sync games man lp mail news uucp proxy www-data backup list irc gnats nobody _apt systemd—tymesync avahi-autoipd systemd—coredump"
