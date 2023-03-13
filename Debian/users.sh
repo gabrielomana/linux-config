5e2ce0bc0f755e57a27f191c3fa8c449b2e8a40d
gabrielomana <gabrielomana@gmail.com>
2023-03-13 12:48:49 +0100

    Fix
    Fix

--- Debian/users.sh
+++ Debian/users.sh
@@ -2,12 +2,19 @@
 dir="$(pwd)"
 
 apt install curl wget apt-transport-https dirmngr apt-xapian-index software-properties-common ca-certificates gnupg dialog netselect-apt tree bash-completion util-linux build-essential dkms apt-transport-https bash-completion console-setup curl debian-reference-es linux-base lsb-release make man-db manpages memtest86+ gnupg linux-headers-$(uname -r) coreutils dos2unix systemd-sysv usbutils unrar-free zip rsync p7zip net-tools screen sudo
-sleep 10
-update-apt-xapian-index -vf
+sleep 5
+
 cp ${dir}/dotfiles/2-sources.list /etc/apt/sources.list -rf
 apt update
-sleep 10
+sleep 5
 
-users_local=$(cut -d: -f1 /etc/passwd)
-user_sys="root daemon bin sys sync games man lp mail news uucp proxy www-data backup list irc gnats nobody _apt systemd—tymesync avahi-autoipd systemd—coredump"
-comm -2 $users_local $user_sys
+user_sys=("root" "daemon" "bin2" "sys" "sync" "games" "man" "lp" "mail" "news" "uucp" "proxy" "www-data" "backup" "list" "irc" "gnats" "nobody" "_apt" "systemd—tymesync" "avahi-autoipd" "systemd—coredump")
+users_local=()
+
+temp=$(cut -d: -f1 /etc/passwd)
+
+for val in $temp; do
+users_local+=($val)
+done
+
+echo ${user_sys[@]} ${users_local[@]} | tr ' ' '\n' | sort | uniq -u
