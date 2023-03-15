cb6ca8ad30d9c6cbd708545006f41226ba90b175
gabrielomana <gabrielomana@gmail.com>
2023-03-15 15:46:13 +0100

    Fix
    Fix

--- Debian/1-pre-install.sh
+++ Debian/1-pre-install.sh
@@ -11,7 +11,7 @@ echo "BASICS PACKEGES"
 sleep 3
 dir="$(pwd)"
 
-apt install aptitude curl wget apt-transport-https dirmngr apt-xapian-index software-properties-common ca-certificates gnupg dialog netselect-apt tree bash-completion util-linux build-essential dkms apt-transport-https bash-completion console-setup curl debian-reference-es linux-base lsb-release make man-db manpages memtest86+ gnupg linux-headers-$(uname -r) coreutils dos2unix systemd-sysv usbutils unrar-free zip rsync p7zip net-tools lz4 screen sudo neofetch isenkram-cli apt-listbugs apt-listchanges -y
+apt install aptitude curl wget apt-transport-https dirmngr apt-xapian-index software-properties-common ca-certificates gnupg dialog netselect-apt tree bash-completion util-linux build-essential dkms apt-transport-https bash-completion console-setup curl debian-reference-es linux-base lsb-release make man-db manpages memtest86+ gnupg linux-headers-$(uname -r) coreutils dos2unix systemd-sysv usbutils unrar-free zip rsync p7zip net-tools lz4 screen sudo neofetch isenkram-cli apt-listbugs apt-listchanges gpgv -y
 
 sleep 5
 
@@ -42,13 +42,16 @@ if [ $r == 1 ]; then
     cp ${dir}/dotfiles/1-sources.list /etc/apt/sources.list -rf
 
     apt-get update -oAcquire::AllowInsecureRepositories=true
+    wget https://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
     apt-get install deb-multimedia-keyring -yy
+    apt install ./deb-multimedia-keyring_2016.8.1_all.deb -y
 
     apt update
     apt upgrade -yy
     apt full-upgrade -yy
 
 elif [ $r == 2 ]; then
+    clear
     cp ${dir}/dotfiles/2-sources.list /etc/apt/sources.list -rf
     deb_cn=$(curl -s https://deb.debian.org/debian/dists/stable/Release | grep ^Codename: | tail -n1 | awk '{print $2}')
     deb_cn="$(echo "$deb_cn" | tr -d ' ')"
@@ -83,9 +86,14 @@ elif [ $r == 2 ]; then
     mv /etc/apt/trusted.gpg /etc/apt/mx.gpg
     ln -s /etc/apt/mx.gpg /etc/apt/trusted.gpg.d/mx.gpg
     fi
+    sleep 10
+    clear
 
+    apt-get update --allow-releaseinfo-change
     apt-get update -oAcquire::AllowInsecureRepositories=true
     apt-get install deb-multimedia-keyring -yy
+    apt-get update
+    sleep 10
 
     apt update
     apt upgrade -yy
@@ -96,7 +104,9 @@ elif [ $r == 3 ]; then
     cp ${dir}/dotfiles/3-sources.list /etc/apt/sources.list -rf
 
     apt-get update -oAcquire::AllowInsecureRepositories=true
+    wget https://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
     apt-get install deb-multimedia-keyring -yy
+    apt install ./deb-multimedia-keyring_2016.8.1_all.deb -y
 
     apt update
     apt upgrade -yy
@@ -147,7 +157,7 @@ for i in "${!users_system[@]}"; do
 #begin looping through users_system
     for x in "${!users_default[@]}";
 #compare the two items
-        if test "${users_system[i]}"  == "${users_default[x]}"; then
+        if test ()"${users_system[i]}"  == "${users_default[x]}"; then
 #add item to the common_list, then remove it from users_default and users_system so that we can
 #later use those to generate the diff_list
             unset 'users_default[x]'
