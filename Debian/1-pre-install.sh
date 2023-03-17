f680a0a0326d70c5d245e05fc836881e46b362be
gabrielomana <gabrielomana@gmail.com>
2023-03-17 17:01:20 +0100

    Fix
    Fix

--- Debian/1-pre-install.sh
+++ Debian/1-pre-install.sh
@@ -29,7 +29,7 @@ while [ $a -lt 1 ]
 do
         echo "SELECT THE DEBIAN BRANCH YOU WANT TO INSTALL:"
         echo "  1)Debian Stable"
-        echo "  2)Debian Stable+Backports+MX repos"
+        echo "  2)Debian Stable+Backports"
         echo "  3)Debian Testing"
         read -p "> " b
 
@@ -74,41 +74,8 @@ elif [ $r == 2 ]; then
     deb_cn=$(curl -s https://deb.debian.org/debian/dists/stable/Release | grep ^Codename: | tail -n1 | awk '{print $2}')
     deb_cn="$(echo "$deb_cn" | tr -d ' ')"
 
+
     echo -e "deb https://deb.debian.org/debian/ $deb_cn-backports main" | sudo tee -a /etc/apt/sources.list.d/debian-backports.list
-
-    apt clean
-    apt update
-    sleep 5
-    clear
-
-#     echo "MX REPOS"
-#     echo -e "deb https://mxrepo.com/mx/repo/ $deb_cn main non-free" | sudo tee -a /etc/apt/sources.list.d/mx.list
-#
-#     curl -s https://mxrepo.com/mx27repo.asc | apt-key add -
-#     if test -f "/etc/apt/trusted.gpg"; then
-#     mv /etc/apt/trusted.gpg /etc/apt/mx.gpg
-#     ln -s /etc/apt/mx.gpg /etc/apt/trusted.gpg.d/mx.gpg
-#     sleep 5
-#     echo " "
-#     echo " "
-#     fi
-#     curl -s https://mxrepo.com/mx25repo.asc | apt-key add -
-#     if test -f "/etc/apt/trusted.gpg"; then
-#     mv /etc/apt/trusted.gpg /etc/apt/mx.gpg
-#     ln -s /etc/apt/mx.gpg /etc/apt/trusted.gpg.d/mx.gpg
-#     fi
-#
-#     curl -s https://mxrepo.com/mx23repo.asc | apt-key add -
-#     if test -f "/etc/apt/trusted.gpg"; then
-#     mv /etc/apt/trusted.gpg /etc/apt/mx.gpg
-#     ln -s /etc/apt/mx.gpg /etc/apt/trusted.gpg.d/mx.gpg
-#     fi
-#
-#     curl -s https://mxrepo.com/mx21repo.asc | apt-key add -
-#     if test -f "/etc/apt/trusted.gpg"; then
-#     mv /etc/apt/trusted.gpg /etc/apt/mx.gpg
-#     ln -s /etc/apt/mx.gpg /etc/apt/trusted.gpg.d/mx.gpg
-#     fi
 
     apt clean
     apt update
@@ -132,8 +99,8 @@ elif [ $r == 2 ]; then
 
     echo "FULL UPGRADE"
     #apt -t $deb_cn-backports upgrade -yy
-    apt upgrade -y
-    apt full-upgrade -y
+    apt upgrade -yy
+    apt full-upgrade -yy
 
 elif [ $r == 3 ]; then
     cp ${dir}/dotfiles/testing_sources.list /etc/apt/sources.list -rf
