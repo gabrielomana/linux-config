e2c54f49949e0c8dc2a4e21b99aef2c0895b5d28
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-08-30 14:23:09 +0200

    Fix
    Fix

--- SparkyLinux/1 - pre-install.sh
+++ SparkyLinux/1 - pre-install.sh
@@ -1,136 +1,95 @@
 #!/bin/bash
-# if [ "$(whoami)" != "root" ]
-# then
-#     sudo su -s "$0"
-#     exit
-# fi
-date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f2-7)"
 
-###################### LANGUAGE ###############################
+# Comprueba si el usuario es root, si no lo es, ejecuta el script como root
+if [ "$(whoami)" != "root" ]; then
+    echo "Este script debe ser ejecutado como root."
+    exit 1
+fi
+
+# Sincroniza la fecha con un servidor NTP confiable
+sudo ntpdate pool.ntp.org
+
+# Instalación y configuración de idioma y locales
+sudo apt update
 sudo apt install locales -y
 sudo apt-get install locales-all -y
 sudo apt-get install language-pack-es -y
 sudo locale-gen "es_ES.UTF-8"
 sudo apt install hunspell-es -y
-sudo setxkbmap -layout 'es,es'
-export LC_ALL="es_ES.UTF-8"
-export LANGUAGE=\"es_ES.UTF-8\"
-export LC_CTYPE="es_ES.UTF-8"
-export LC_NUMERIC="es_ES.UTF-8"
-export LC_TIME="es_ES.UTF-8"
-export LC_COLLATE="es_ES.UTF-8"
-export LC_MONETARY="es_ES.UTF-8"
-export LC_MESSAGES="es_ES.UTF-8"
-export LC_PAPER="es_ES.UTF-8"
-export LC_NAME="es_ES.UTF-8"
-export LC_ADDRESS="es_ES.UTF-8"
-export LC_TELEPHONE="es_ES.UTF-8"
-export LC_MEASUREMENT="es_ES.UTF-8"
-export LC_IDENTIFICATION="es_ES.UTF-8"
+sudo localectl set-x11-keymap es,es
+sudo update-locale LANG=es_ES.UTF-8
+source /etc/default/locale
 
-###################### UPDATE ###############################
+# Actualización del sistema
 sudo apt update
 sudo apt full-upgrade -y
 sudo dpkg --configure -a
 sudo apt install -f
 sudo apt autoremove
 sudo apt clean
-sudo apt update
 sudo apt --fix-broken install
 sudo aptitude safe-upgrade -y
 sudo apt install linux-headers-$(uname -r) -y
 
-
-##NALA
+# Instalación y actualización de NALA (si es necesario)
 sudo apt install nala -y
 sudo nala fetch --auto --fetches 5 -y
-sudo nala update; sudo nala upgrade -y; sudo nala install -f;
+sudo nala update
+sudo nala upgrade -y
+sudo nala install -f
 
-###################### BASICS PACKEGES ###############################
+# Instalación de paquetes básicos
 clear
-echo "BASICS PACKEGES"
+echo "BASIC PACKAGES"
 sleep 3
 dir="$(pwd)"
-
 sudo apt install aptitude curl wget apt-transport-https dirmngr lz4 sudo gpgv gnupg devscripts systemd-sysv software-properties-common ca-certificates dialog dkms cmake build-essential python3-pip pipx -y
-sleep 10
+sleep 5
 clear
 
-##Pipewire & Wireplumber
-sudo apt install libfdk-aac2 libldacbt-{abr,enc}2 libopenaptx0 install gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules} libspa-0.2-{bluetooth,dev,jack,modules} pipewire{,-{audio-client-libraries,pulse,bin,locales,tests}} pipewire-doc libpipewire-module-x11-bell wireplumber{,-doc} gir1.2-wp-0.4 libwireplumber-0.4-{0,dev} wireplumber-locales pipewire pipewire-audio-client-libraries pipewire-media-session-* libspa-0.2-bluetooth -y
+# Instalación de Pipewire y Wireplumber
+sudo apt install libfdk-aac2 libldacbt-{abr,enc}2 libopenaptx0 gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules} libspa-0.2-{bluetooth,dev,jack,modules} pipewire{,-{audio-client-libraries,pulse,bin,tests}} pipewire-doc libpipewire-* wireplumber{,-doc} gir1.2-wp-0.4 libwireplumber-0.4-{0,dev} pipewire pipewire-audio-client-libraries pipewire-media-session-* libspa-0.2-bluetooth -y
 sudo touch /etc/pipewire/media-session.d/with-pulseaudio
 sudo cp /usr/share/doc/pipewire/examples/systemd/user/pipewire-pulse.* /etc/systemd/user/
 systemctl --user --now disable pulseaudio.{socket,service}
 systemctl --user mask pulseaudio
 systemctl --user --now enable pipewire{,-pulse}.{socket,service}
 systemctl --user --now enable wireplumber.service
-sleep 10
+sleep 5
 clear
 
-echo "OTHER BASICS PACKEGES"
+# Instalación de otros paquetes básicos
+echo "OTHER BASIC PACKAGES"
 sleep 3
-
-sudo nala install apt-xapian-index netselect-apt tree bash-completion util-linux build-essential console-setup debian-reference-es linux-base lsb-release make man-db manpages memtest86+ coreutils dos2unix usbutils bleachbit python3-venv python3-pip unrar-free zip rsync p7zip net-tools screen neofetch -y
-sleep 10
+sudo nala install apt-xapian-index netselect-apt tree bash-completion util-linux debian-reference-es linux-base lsb-release make man-db manpages memtest86+ coreutils dos2unix usbutils bleachbit python3-venv python3-pip unrar-free zip rsync p7zip net-tools screen neofetch -y
+sleep 5
 clear
 
-####################### ZSWAP+SWAPPINESS+GRUB ###############################
+# Configuración de Zswap, swappiness y actualización de Grub
 clear
 echo "ZSWAP+SWAPPINESS+GRUB"
 sleep 3
-echo -e "vm.swappiness=25" | sudo tee -a /etc/sysctl.conf
-
+echo "vm.swappiness=25" | sudo tee -a /etc/sysctl.conf
 sudo cp /etc/default/grub /etc/default/grub_old
-sudo cp ${dir}/dotfiles/grub /etc/default/grub
+sudo cp "${dir}/dotfiles/grub" /etc/default/grub
 sudo update-grub
-
-echo -e 'lz4' | sudo tee -a /etc/initramfs-tools/modules
-echo -e 'lz4_compress' | sudo tee -a /etc/initramfs-tools/modules
-echo -e 'z3fold' | sudo tee -a /etc/initramfs-tools/modules
-
+echo "lz4" | sudo tee -a /etc/initramfs-tools/modules
+echo "lz4_compress" | sudo tee -a /etc/initramfs-tools/modules
+echo "z3fold" | sudo tee -a /etc/initramfs-tools/modules
 sudo update-initramfs -u
 
-###################### BRANCH ROLLING ###############################
+# Reinicio opcional a la rama "Rolling"
 clear
 a=0
 f=0
-while [ $a -lt 1 ]
-do
-    read -p "Do you want to move to the Rolling branch?? " yn
+while [ $a -lt 1 ]; do
+    read -p "¿Quieres cambiar a la rama Rolling? " yn
     case $yn in
-        [Yy]* ) a=1;rolling_branch;f=1;clear;;
-        [Nn]* ) a=1;echo "OK";clear;;
-        * ) echo "Please answer yes or no.";;
+        [Yy]* ) a=1; rolling_branch; f=1; clear;;
+        [Nn]* ) a=1; echo "OK"; clear;;
+        * ) echo "Por favor, responde sí o no.";;
     esac
 done
 
-clear
-if [ $f == 1 ]; then
-    sudo wget https://sparkylinux.org/files/sparky-dist-upgrade78
-    sudo chmod +x sparky-dist-upgrade78
-    sudo ./sparky-dist-upgrade78
-    sudo apt autoremove
-    sudo apt clean
-    sudo apt update
-    sudo apt --fix-broken install
-    sudo aptitude safe-upgrade -y
-    sudo rm sparky-dist-upgrade78* -rf
-
-    sudo rm /etc/apt/sources.list -f
-    echo -e "deb http://deb.debian.org/debian/ testing main contrib non-free non-free-firmware
-    deb-src http://deb.debian.org/debian/ testing main contrib non-free non-free-firmware
-    deb http://security.debian.org/debian-security/ trixie-security/updates main contrib non-free non-free-firmware
-    deb-src http://security.debian.org/debian-security/ trixie-security/updates main contrib non-free non-free-firmware
-    deb http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
-    deb-src http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
-    deb http://deb-multimedia.org/ testing main non-free"  | sudo tee -a /etc/apt/sources.list
-    sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
-    sudo bleachbit -c apt.autoclean apt.autoremove apt.clean system.tmp system.trash system.cache system.localizations system.desktop_entry
-    sleep 3
-    sudo nala update
-    sudo nala fetch --auto --fetches 5 -y
-    sudo nala update; sudo nala upgrade -y; sudo nala install -f; sudo apt --fix-broken install
-    clear
-fi
-clear
+# Reiniciar el sistema
 sudo reboot
--- SparkyLinux/KDE_PLASMA/3-style.sh
+++ SparkyLinux/KDE_PLASMA/3-style.sh
@@ -6,11 +6,11 @@ then
 fi
 dir="$(pwd)"
 
-exta_apps="${dir}/sources/lists/exta_apps.list"
+extra_apps="${dir}/sources/lists/extra_apps.list"
 
-. "${dir}"/sources/functions/zsh_starship
+. "${dir}/sources/functions/zsh_starship"
 
-#POP_OS ICONS
+# POP_OS ICONS
 clear
 echo "POP_OS! ICONS"
 wget https://github.com/gabrielomana/Pop_Os-Icons/raw/main/KDE/Pop_Os-Icons.tar.gz
@@ -18,17 +18,15 @@ sudo tar -xvf Pop_Os-Icons.tar.gz -C /usr/share/icons/
 rm Pop_Os-Icons.tar.gz -rf
 sleep 3
 
-
-#Theme Windows
+# Theme Windows
 clear
 echo "Theme: Win11OS"
-
 git clone https://github.com/yeyushengfan258/Win11OS-kde.git /git/Win11OS-kde
 cd /git/Win11OS-kde
 ./install.sh
 sleep 3
 
-#Win10OS-cursors
+# Win10OS-cursors
 clear
 echo "Win10OS-cursors"
 git clone https://github.com/yeyushengfan258/Win10OS-cursors.git /git/Win10OS-cursors/
@@ -36,7 +34,7 @@ cd /git/Win10OS-cursors/
 ./install.sh
 sleep 3
 
-##phinger-cursors
+# phinger-cursors
 clear
 echo "INSTALL phinger-cursors"
 wget https://github.com/phisch/phinger-cursors/releases/latest/download/phinger-cursors-variants.tar.bz2
@@ -44,10 +42,9 @@ sudo tar -xjvf phinger-cursors-variants.tar.bz2 -C /usr/share/icons/
 rm phinger-cursors-variants.tar.bz2 -rf
 sleep 3
 
-#Orchis Theme
+# Orchis Theme
 clear
 echo "Theme: Orchis Theme"
-
 git clone https://github.com/vinceliuice/Orchis-kde.git /git/Orchis-kde
 cd /git/Orchis-kde
 ./install.sh
@@ -57,8 +54,7 @@ cd /git/Orchis-theme
 ./install.sh
 sleep 3
 
-
-##Application Style: Klassy
+# Application Style: Klassy
 clear
 echo "Application Style: Klassy"
 sudo nala install build-essential libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev extra-cmake-modules libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev gettext cmake libkf5iconthemes-dev libkf5package-dev libkf5style-dev libkf5kcmutils-dev kirigami2-dev -y
@@ -68,12 +64,10 @@ cd /git/Klassy
 sudo ./install.sh
 sleep 3
 
-
-#PAPIRUS
+# PAPIRUS
 clear
 echo "INSTALL PAPIRUS"
 sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu focal main' > /etc/apt/sources.list.d/papirus-ppa.list"
-
 sudo apt-get install dirmngr -y
 sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/papirus.gpg --keyserver keyserver.ubuntu.com --recv E58A9D36647CAE7F
 sudo chmod 644 /etc/apt/trusted.gpg.d/papirus.gpg
@@ -84,7 +78,7 @@ okular ${dir}/customization_guide.pdf
 sudo bleachbit -c apt.autoclean apt.autoremove apt.clean system.tmp system.trash system.cache system.localizations system.desktop_entry
 sudo nala update; sudo nala upgrade -y; sudo nala install -f;
 
-####### DOTFILES+ZSH+OHMYZSH+STARSHIP #############################################
+# DOTFILES+ZSH+OHMYZSH+STARSHIP
 clear
 echo "ZSH"
 sleep 3
@@ -93,15 +87,3 @@ cd ${dir}
 a=0
 f=0
 install_ZSH
-# while [ $a == 0 ]
-# do
-#         read -p "Do you wish to install ZSH+OHMYZSH+STARSHIP? " yn
-#         case $yn in
-#             [Yy]* ) a=1;install_ZSH;clear;;
-#             [Nn]* ) a=1;echo "OK";clear;;
-#             * ) echo "Please answer yes or no.";;
-#         esac
-#     done
-
-
-
