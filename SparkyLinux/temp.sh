2796505329c240d19330551b4a0633e161aab104
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-08-30 16:25:16 +0200

    Fix
    Fix

--- SparkyLinux/temp.sh
+++ SparkyLinux/temp.sh
@@ -87,5 +87,74 @@ while [ $a -lt 1 ]; do
     esac
 done
 
+if [ $f == 1 ]; then
+ DEPS="bash coreutils dialog grep iputils-ping sparky-info sudo"
+
+  PINGTEST0=$(ping -c 1 debian.org | grep [0-9])
+  if [ -z "$PINGTEST0" ]; then
+    echo "Debian server is offline... exiting..."
+    exit 1
+  fi
+
+  PINGTEST1=$(ping -c 1 sparkylinux.org | grep [0-9])
+  if [ -z "$PINGTEST1" ]; then
+    echo "Sparky server is offline... exiting..."
+    exit 1
+  fi
+
+  OSCODE=$(grep Orion /etc/lsb-release)
+  if [ -z "$OSCODE" ]; then
+    echo "This is not Sparky 7 Orion Belt... exiting..."
+    exit 1
+  fi
+
+  sudo rm -f /etc/apt/sources.list
+  echo -e "deb http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security/ trixie-security/updates main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security/ trixie-security/updates main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian trixie-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian trixie-updates main contrib non-free non-free-firmware
+deb http://deb-multimedia.org/ trixie main non-free" | sudo tee -a /etc/apt/sources.list
+
+  sudo rm -f /etc/apt/sources.list.d/sparky.list
+  echo -e "deb https://repo.sparkylinux.org/ core main
+deb-src https://repo.sparkylinux.org/ core main
+deb https://repo.sparkylinux.org/ sisters main
+deb-src https://repo.sparkylinux.org/ sisters main" | sudo tee -a /etc/apt/sources.list.d/sparky.list
+
+  sudo apt update
+  sudo apt full-upgrade
+  sudo dpkg --configure -a
+  sudo apt install -f
+
+  sudo apt autoremove
+  sudo apt clean
+  sudo apt update
+  sudo apt --fix-broken install
+  sudo aptitude safe-upgrade -y
+
+  # Move to testing
+  clear
+  sudo rm /etc/apt/sources.list -f
+  echo -e "deb https://deb.debian.org/debian/ testing main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian/ testing main contrib non-free non-free-firmware
+deb https://security.debian.org/debian-security/ trixie-security/updates main contrib non-free non-free-firmware
+deb-src https://security.debian.org/debian-security/ trixie-security/updates main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
+deb-src https://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
+deb https://deb-multimedia.org/ testing main non-free" | sudo tee -a /etc/apt/sources.list
+
+  sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+  sudo bleachbit -c apt.autoclean apt.autoremove apt.clean system.tmp system.trash system.cache system.localizations system.desktop_entry
+  sleep 3
+  sudo nala update
+  sudo nala fetch --auto --fetches 5 -y
+  sudo nala update && sudo nala upgrade -y && sudo nala install -f && sudo apt --fix-broken install
+  clear
+
+fi
+return
+
 # Reiniciar el sistema
 #sudo reboot
