7d5b59362944a7b7794ce3957d9e64bed1c6807e
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-12-08 00:53:21 +0100

    Fix
    Fix

--- Mint2/KDE_PLASMA/2-post-install.sh
+++ Mint2/KDE_PLASMA/2-post-install.sh
@@ -79,7 +79,38 @@ sudo apt update && sudo apt install linux-xanmod-x64v3
 sudo apt update
 sudo sh -c 'echo "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Download-Upgradeable-Packages \"0\";\nAPT::Periodic::AutocleanInterval \"0\";\nAPT::Periodic::Unattended-Upgrade \"1\";" > /etc/apt/apt.conf.d/10periodic'
 sudo sh -c 'echo "Unattended-Upgrade::Allowed-Origins {\n\t\"${distro_id}:${distro_codename}-security\";\n\t\"${distro_id}:${distro_codename}-updates\";\n\t\"${distro_id}ESM:${distro_codename}\";\n};" > /etc/apt/apt.conf.d/50unattended-upgrades'
-sudo apt install linux-xanmod-lts -y
+
+BEGIN {
+    while ((getline < "/proc/cpuinfo") > 0) {
+        if (!/flags/) {
+            exit 1
+        }
+    }
+    if (/lm/ && /cmov/ && /cx8/ && /fpu/ && /fxsr/ && /mmx/ && /syscall/ && /sse2/) {
+        level = 1
+    }
+    if (level == 1 && /cx16/ && /lahf/ && /popcnt/ && /sse4_1/ && /sse4_2/ && /ssse3/) {
+        level = 2
+    }
+    if (level == 2 && /avx/ && /avx2/ && /bmi1/ && /bmi2/ && /f16c/ && /fma/ && /abm/ && /movbe/ && /xsave/) {
+        level = 3
+    }
+    if (level == 3 && /avx512f/ && /avx512bw/ && /avx512cd/ && /avx512dq/ && /avx512vl/) {
+        level = 4
+    }
+    if (level > 0) {
+        ver = level
+        exit level + 1
+    }
+    exit 1
+}
+
+# Append "v" to the version for the package name
+ver = "v"ver
+
+# Install the package using apt
+system("sudo apt install linux-xanmod-lts" ver " -y")
+
 sudo update-initramfs -u
 
 
