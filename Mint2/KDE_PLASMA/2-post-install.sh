03ef3d10b2e60bdf5b5bc5f196431bd78df0b7d4
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-12-07 20:13:35 +0100

    Fix
    Fix

--- Mint2/KDE_PLASMA/2-post-install.sh
+++ Mint2/KDE_PLASMA/2-post-install.sh
@@ -12,6 +12,30 @@ clear
 echo "KONSOLE & DOTFILES"
 sleep 3
 
+dir="$(pwd)"
+sudo git clone https://github.com/fastfetch-cli/fastfetch.git /git/linux-config/SparkyLinux/KDE_PLASMA/fastfetch/
+cd /git/linux-config/SparkyLinux/KDE_PLASMA/fastfetch/
+sudo mkdir -p build
+cd build
+sudo cmake ..
+sudo cmake --build . --target fastfetch --target flashfetch
+sudo cp fastfetch flashfetch /usr/bin/
+cd ${dir}
+fastfetch --gen-config-force
+cp -r dotfiles/fastfetch_config.jsonc ~/.config/fastfetch/config.jsonc
+cp -r dotfiles/sparky ~/.config/fastfetch/sparky
+
+sudo wget https://github.com/gabrielomana/color_schemes/raw/main/konsole.zip
+sudo unzip konsole.zip
+sudo cp konsole/* /usr/share/konsole/ -rf
+sudo rm konsole/ -rf
+
+cp -r dotfiles/neofetch.conf ~/.config/neofetch/config.conf
+cp -r dotfiles/topgrade.toml ~/.config/topgrade.toml
+cp -r dotfiles/.nanorc ~/.config/.nanorc
+cp -r dotfiles/konsole.profile ~/.local/share/konsole/konsole.profile
+cp -r dotfiles/konsolerc ~/.config/konsolerc
+
 # Descargar y configurar esquemas de colores para Konsole
 sudo wget -q https://github.com/gabrielomana/color_schemes/raw/main/konsole.zip -P /tmp
 sudo unzip -q /tmp/konsole.zip -d /tmp
