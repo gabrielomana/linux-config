9f11449ff612178b0e2e8d0be965120cb0bbfe28
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-12-28 10:24:01 +0100

    fix
    fix

--- Mint2/KDE_PLASMA/check_pkg.sh
+++ Mint2/KDE_PLASMA/check_pkg.sh
@@ -1,79 +1,86 @@
 #!/bin/bash
+
 function check_installed {
   local package
   local to_install=()
   list=""
   list2=""
+  
   while IFS= read -r package; do
     [ -z "${package}" ] && continue
 
     STR="${package}"
-    SUB='*'
 
-    case $STR in
-      *"$SUB"*)
-        list2="${list2} \"${STR}\""
-        ;;
-      *)
+    if dpkg -s "${STR}" >/dev/null 2>&1; then
+      echo "El paquete ${STR} ya está instalado."
+    else
+      if nala show "${STR}" 2>&1 | grep -q "Error: "; then
+        echo "El paquete ${STR} no está instalado y no se puede instalar."
+      else
         list="${list} \"${STR}\""
-        ;;
-    esac
-
-    if !(dpkg -s "${package}" >/dev/null 2>&1); then
-      if !(nala show "${package}" 2>&1 | grep -q "Error: "); then
-        list="${list} \"${package}\""
+        list2="${list2} \"${STR}\""
       fi
     fi
   done < "${1}"
 
-  c="sudo nala install ${list} -y"
-  c2="sudo apt install ${list2} -y"
-  echo $c
-  echo $c2
+  # Imprime los paquetes a instalar con nala
+  if [ -n "${list}" ]; then
+    echo "Paquetes a instalar con nala:"
+    echo "sudo nala install ${list} -y"
+  else
+    echo "No hay paquetes para instalar con nala."
+  fi
+
+  # Imprime los paquetes a instalar con apt
+  if [ -n "${list2}" ]; then
+    echo "Paquetes a instalar con apt:"
+    echo "sudo apt install ${list2} -y"
+  else
+    echo "No hay paquetes para instalar con apt."
+  fi
 }
 
-package=("kcalc" "kate" "kmix" "knotes" "\"kde-config-cron*\"" "krename" "kamoso" "kolourpaint" "kid3" "kcolorchooser"
-  "kcharselect" "kdenetwork-filesharing" "kfind" "kget" "kinfocenter" "\"kio*\"" "\"kio*\"" "kio-admin" "kleopatra"
-  "krdc" "kaccounts-providers" "kio-gdrive" "kbackup" "plasma-nm" "plasma-pa" "\"plasma-widget*\"" "\"plasma-widget*\""
-  "plasma-widgets-addons" "ffmpegthumbs" "ark" "okular" "ksystemlog" "kde-config-cron" "kdeplasma-addons" "\"kdeplasma-addon*\"")
-
-package_2=(
-"kcalc"
-"kate"
-"kmix"
-"knotes"
-"\"kde-config-cron*\""
-"krename"
-"kamoso"
-"kolourpaint"
-"kid3"
-"kcolorchooser"
-"kcharselect"
-"kdenetwork-filesharing"
-"kfind"
-"kget"
-"kinfocenter"
-"\"kio*\""
-"\"kio*\""
-"kio-admin"
-"kleopatra"
-"krdc"
-"kaccounts-providers"
-"kio-gdrive"
-"kbackup"
-"plasma-nm"
-"plasma-pa"
-"\"plasma-widget*\""
-"\"plasma-widget*\""
-"plasma-widgets-addons"
-"ffmpegthumbs"
-"ark"
-"okular"
-"ksystemlog"
-"kde-config-cron"
-"kdeplasma-addons"
-"\"kdeplasma-addon*\""
-)
-
+# Ejemplo de lista de paquetes
+package=("kcalc" "kate" "kmix" "knotes" "kde-config-cron*" "krename" "kamoso" "kolourpaint" "kid3" "kcolorchooser"
+  "kcharselect" "kdenetwork-filesharing" "kfind" "kget" "kinfocenter" "kio*" "kio-admin" "kleopatra"
+  "krdc" "kaccounts-providers" "kio-gdrive" "kbackup" "plasma-nm" "plasma-pa" "plasma-widget*" "plasma-widgets-addons"
+  "ffmpegthumbs" "ark" "okular" "ksystemlog" "kde-config-cron" "kdeplasma-addons" "kdeplasma-addon*")
+# package=(
+#   "kcalc"
+#   "kate"
+#   "kmix"
+#   "knotes"
+#   "\"kde-config-cron*\""
+#   "krename"
+#   "kamoso"
+#   "kolourpaint"
+#   "kid3"
+#   "kcolorchooser"
+#   "kcharselect"
+#   "kdenetwork-filesharing"
+#   "kfind"
+#   "kget"
+#   "kinfocenter"
+#   "\"kio*\""
+#   "\"kio*\""
+#   "kio-admin"
+#   "kleopatra"
+#   "krdc"
+#   "kaccounts-providers"
+#   "kio-gdrive"
+#   "kbackup"
+#   "plasma-nm"
+#   "plasma-pa"
+#   "\"plasma-widget*\""
+#   "\"plasma-widget*\""
+#   "plasma-widgets-addons"
+#   "ffmpegthumbs"
+#   "ark"
+#   "okular"
+#   "ksystemlog"
+#   "kde-config-cron"
+#   "kdeplasma-addons"
+#   "\"kdeplasma-addon*\""
+# )
 
 check_installed "${package}"
