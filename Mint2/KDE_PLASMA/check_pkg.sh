3ac6f87df06b8af19cdc4561bb0d6d0beac12312
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-12-28 10:40:36 +0100

    fix
    fix

--- Mint2/KDE_PLASMA/check_pkg.sh
+++ Mint2/KDE_PLASMA/check_pkg.sh
@@ -8,13 +8,24 @@ function check_installed {
   while IFS= read -r package; do
     [ -z "${package}" ] && continue
 
-    if dpkg-query -W -f='${Status}\n' "${package}" 2>/dev/null | grep -q "install ok installed"; then
+    if dpkg -l "${package}" 2>/dev/null | grep -q "ii"; then
       echo "El paquete ${package} ya está instalado."
     else
+      # Verificar si el paquete es un patrón y obtener la lista de paquetes que coinciden
       if [[ "${package}" == *'*'* ]]; then
-        list2="${list2} ${package}"
+        matching_packages=($(apt list "${package}" 2>/dev/null | grep -oP "^\S+"))
+        if [ ${#matching_packages[@]} -eq 0 ]; then
+          echo "No hay paquetes que coincidan con el patrón ${package} en los repositorios."
+        else
+          list2="${list2} ${matching_packages[@]}"
+        fi
       else
-        list="${list} ${package}"
+        # Verificar si el paquete existe en los repositorios
+        if apt show "${package}" &>/dev/null; then
+          list="${list} ${package}"
+        else
+          echo "El paquete ${package} no está instalado y no existe en los repositorios."
+        fi
       fi
     fi
   done < "${1}"
@@ -22,7 +33,7 @@ function check_installed {
   # Imprime los paquetes a instalar con nala
   if [ -n "${list}" ]; then
     echo "Paquetes a instalar con nala:"
-    echo "sudo nala install${list} -y"
+    echo "sudo nala install ${list} -y"
   else
     echo "No hay paquetes para instalar con nala."
   fi
@@ -30,7 +41,7 @@ function check_installed {
   # Imprime los paquetes a instalar con apt
   if [ -n "${list2}" ]; then
     echo "Paquetes a instalar con apt:"
-    echo "sudo apt install${list2} -y"
+    echo "sudo apt install ${list2} -y"
   else
     echo "No hay paquetes para instalar con apt."
   fi
@@ -43,6 +54,7 @@ package=("kcalc" "kate" "kmix" "knotes" "kde-config-cron*" "krename" "kamoso" "k
   "ffmpegthumbs" "ark" "okular" "ksystemlog" "kde-config-cron" "kdeplasma-addons" "kdeplasma-addon*")
 
 check_installed "${package}"
+
 
 
 # package=(
