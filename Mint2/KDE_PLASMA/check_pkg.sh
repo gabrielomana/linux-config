8fbe90872e28c2a6ddeea67e45fc6446d8d1f473
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2023-12-28 10:08:46 +0100

    fix
    fix

--- Mint2/KDE_PLASMA/check_pkg.sh
+++ Mint2/KDE_PLASMA/check_pkg.sh
@@ -11,13 +11,16 @@ function check_installed {
 
     case $STR in
       *"$SUB"*)
-        list2="${list2} ${STR}"
+        list2="${list2} \"${STR}\""
+        ;;
+      *)
+        list="${list} \"${STR}\""
         ;;
     esac
 
     if !(dpkg -s "${package}" >/dev/null 2>&1); then
       if !(nala show "${package}" 2>&1 | grep -q "Error: "); then
-        list="${list} ${package}"
+        list="${list} \"${package}\""
       fi
     fi
   done < "${1}"
@@ -29,41 +32,41 @@ function check_installed {
 }
 
 package=(
-  kcalc
-  kate
-  kmix
-  knotes
-  "kde-config-cron*"
-  krename
-  kamoso
-  kolourpaint
-  kid3
-  kcolorchooser
-  kcharselect
-  kdenetwork-filesharing
-  kfind
-  kget
-  kinfocenter
-  "kio*"
-  "kio*"
-  kio-admin
-  kleopatra
-  krdc
-  kaccounts-providers
-  kio-gdrive
-  kbackup
-  plasma-nm
-  plasma-pa
-  "plasma-widget*"
-  "plasma-widget*"
-  plasma-widgets-addons
-  ffmpegthumbs
-  ark
-  okular
-  ksystemlog
-  kde-config-cron
-  kdeplasma-addons
-  "kdeplasma-addon*"
+  "kcalc"
+  "kate"
+  "kmix"
+  "knotes"
+  "\"kde-config-cron*\""
+  "krename"
+  "kamoso"
+  "kolourpaint"
+  "kid3"
+  "kcolorchooser"
+  "kcharselect"
+  "kdenetwork-filesharing"
+  "kfind"
+  "kget"
+  "kinfocenter"
+  "\"kio*\""
+  "\"kio*\""
+  "kio-admin"
+  "kleopatra"
+  "krdc"
+  "kaccounts-providers"
+  "kio-gdrive"
+  "kbackup"
+  "plasma-nm"
+  "plasma-pa"
+  "\"plasma-widget*\""
+  "\"plasma-widget*\""
+  "plasma-widgets-addons"
+  "ffmpegthumbs"
+  "ark"
+  "okular"
+  "ksystemlog"
+  "kde-config-cron"
+  "kdeplasma-addons"
+  "\"kdeplasma-addon*\""
 )
 
 check_installed "${package}"
