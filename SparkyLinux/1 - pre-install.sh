8ade7857e3d24fa066a4582be144b908b6b4a37c
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2024-01-13 13:03:55 +0100

    FIX

--- SparkyLinux/1 - pre-install.sh
+++ SparkyLinux/1 - pre-install.sh
@@ -1,109 +1,145 @@
 #!/bin/bash
 
-# Guarda el directorio actual en una variable
+############################################
+# UPDATE SYSTEM DATE AND TIME
+############################################
+
+# Save the current directory in a variable
 dir="$(pwd)"
 
-# Importa funciones desde el directorio indicado
-. "${dir}/KDE_PLASMA/sources/functions/functions"
+# Update the system date and time using Google's response
+date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f2-7)"
 
-# # Actualiza la fecha y hora del sistema usando la respuesta de Google
-# date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f2-7)"
+############################################
+# LANGUAGE AND LOCALES SETUP
+############################################
 
-# # SUDO
-# # Instala sudo si no está instalado
-# if ! command -v sudo &> /dev/null; then
-#     echo "sudo not found. Installing sudo..."
-#     apt update
-#     apt install sudo -y
-# fi
+# Install and configure language and locales
+sudo apt install locales locales-all language-pack-es hunspell-es -y
+sudo locale-gen "es_ES.UTF-8"
+sudo localectl set-x11-keymap es,es
+sudo update-locale LANG=es_ES.UTF-8
+source /etc/default/locale
 
-# # Agrega el usuario actual al archivo sudoers
-# echo "$USER ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
+############################################
+# SYSTEM UPDATE
+############################################
 
-# # Verifica si se agregó la línea correctamente
-# grep -q "$USER" /etc/sudoers && echo "User $USER successfully added to the sudoers list." || echo "Error adding user $USER to the sudoers list."
+# Update the system
+sudo apt update
+sudo apt full-upgrade -y
+sudo dpkg --configure -a
+sudo apt install -f
+sudo apt clean
+sudo apt --fix-broken install
+sudo aptitude safe-upgrade -y
+sudo apt install linux-headers-$(uname -r) -y
 
-# # Instalación y configuración de idioma y locales
-# clear
-# sudo nala install locales locales-all language-pack-es hunspell-es -y
-# sudo locale-gen "es_ES.UTF-8"
-# sudo localectl set-x11-keymap es,es
-# sudo update-locale LANG=es_ES.UTF-8
-# source /etc/default/locale
+############################################
+# NALA INSTALLATION AND UPDATE
+############################################
 
-# # Actualización del sistema
-# sudo apt update
-# sudo apt full-upgrade -y
-# sudo dpkg --configure -a
-# sudo apt install -f
-# sudo apt clean
-# sudo apt --fix-broken install
-# sudo aptitude safe-upgrade -y
-# sudo apt install linux-headers-$(uname -r) -y
+# Install NALA if not installed
+if ! command -v nala &> /dev/null; then
+    echo "NALA not found. Installing NALA..."
+    sudo apt install nala -y
+fi
 
-# # Instalación y actualización de NALA (si es necesario)
-# if ! command -v nala &> /dev/null; then
-#     echo "NALA not found. Installing NALA..."
-#     sudo apt install nala -y
-# fi
+# Update NALA if installed
+if command -v nala &> /dev/null; then
+    echo "Updating NALA..."
+    sudo nala fetch --auto --fetches 5 -y
+    sudo nala update
+    sudo nala upgrade -y
+    sudo nala install -f
+else
+    echo "NALA not installed. Skipping update."
+fi
 
-# # Actualiza NALA si está instalado
-# if command -v nala &> /dev/null; then
-#     echo "Updating NALA..."
-#     sudo nala fetch --auto --fetches 5 -y
-#     sudo nala update
-#     sudo nala upgrade -y
-#     sudo nala install -f
-# else
-#     echo "NALA not installed. Skipping update."
-# fi
+############################################
+# BASIC PACKAGES INSTALLATION
+############################################
+ clear
+ echo "BASIC PACKAGES"
+ sleep 3
+ dir="$(pwd)"
+ sudo nala install  -y apt-transport-https \
+  apt-xapian-index \
+  aptitude \
+  bash-completion \
+  bleachbit \
+  build-essential \
+  ca-certificates \
+  coreutils \
+  cmake \
+  curl \
+  debian-reference-es \
+  devscripts \
+  dialog \
+  dirnagr \
+  dkms \
+  dos2unix \
+  dpgv \
+  gpgv \
+  gnupg \
+  linux-base \
+  lsb-release \
+  lz4 \
+  make \
+  man-db \
+  manpages \
+  memtest86+ \
+  net-tools \
+  netselect-apt \
+  neofetch \
+  p7zip \
+  pipx \
+  python3-pip \
+  python3-venv \
+  rsync \
+  screen \
+  software-properties-common \
+  sudo \
+  systemd-sysv \
+  tree \
+  unrar-free \
+  util-linux \
+  usbutils \
+  wget \
+  zip
+ sleep 5
+ clear
 
-# # Instalación de paquetes básicos
-# clear
-# echo "BASIC PACKAGES"
-# sleep 3
-# dir="$(pwd)"
-# sudo nala install aptitude curl wget apt-transport-https dirmngr lz4 sudo gpgv gnupg devscripts systemd-sysv software-properties-common ca-certificates dialog dkms cmake build-essential python3-pip pipx -y
-# sleep 5
-# clear
+############################################
+# ZSWAP, SWAPPINESS, AND GRUB UPDATE
+############################################
 
-# # Instalación de otros paquetes básicos
-# echo "OTHER BASIC PACKAGES"
-# sleep 3
-# sudo nala install apt-xapian-index netselect-apt tree bash-completion util-linux debian-reference-es linux-base lsb-release make man-db manpages memtest86+ coreutils dos2unix usbutils bleachbit python3-venv python3-pip unrar-free zip rsync p7zip net-tools screen neofetch -y
-# sleep 5
-# clear
+# Configure Zswap, swappiness, and update Grub
+echo "vm.swappiness=25" | sudo tee -a /etc/sysctl.conf
+sudo cp /etc/default/grub /etc/default/grub_old
+sudo cp "${dir}/dotfiles/grub" /etc/default/grub
+sudo update-grub
+echo "lz4" | sudo tee -a /etc/initramfs-tools/modules
+echo "lz4_compress" | sudo tee -a /etc/initramfs-tools/modules
+echo "z3fold" | sudo tee -a /etc/initramfs-tools/modules
+sudo update-initramfs -u
 
-# # Configuración de Zswap, swappiness y actualización de Grub
-# clear
-# echo "ZSWAP+SWAPPINESS+GRUB"
-# sleep 3
-# echo "vm.swappiness=25" | sudo tee -a /etc/sysctl.conf
-# sudo cp /etc/default/grub /etc/default/grub_old
-# sudo cp "${dir}/dotfiles/grub" /etc/default/grub
-# sudo update-grub
-# echo "lz4" | sudo tee -a /etc/initramfs-tools/modules
-# echo "lz4_compress" | sudo tee -a /etc/initramfs-tools/modules
-# echo "z3fold" | sudo tee -a /etc/initramfs-tools/modules
-# sudo update-initramfs -u
+############################################
+# OPTIONAL SWITCH TO THE ROLLING BRANCH
+############################################
 
-# Reinicio opcional a la rama "Rolling"
-clear
 a=0
 f=0
-while [ $a -lt 1 ]
-do
-    read -p "¿Quieres cambiar a la rama Rolling?" yn
+while [ $a -lt 1 ]; do
+    read -p "Do you want to switch to the Rolling branch? " yn
     case $yn in
         [Yy]* )
             a=1
             f=1
-            clear
             ;;
         [Nn]* )
             a=1
             echo "OK"
-            clear
             ;;
         * )
             echo "Please answer yes or no."
@@ -217,5 +253,184 @@ if [ $f == 1 ]; then
   
 fi
 
-# Comentario final
-# sudo reboot
+
+############################################
+# GRUB-BTRFS AND TIMESHIFT CONFIGURATION
+############################################
+
+# Check if the root partition is on Btrfs
+if [[ $(df -T / | awk 'NR==2 {print $2}') == "btrfs" ]]; then
+    # Get the UUID of the root partition
+    ROOT_UUID=$(grep -E '/\s+btrfs\s+' "/etc/fstab" | awk '{print $1}' | sed -n 's/UUID=\(.*\)/\1/p')
+
+    # Get the UUID of the home partition
+    HOME_UUID=$(grep -E '/home\s+btrfs\s+' "/etc/fstab" | awk '{print $1}' | sed -n 's/UUID=\(.*\)/\1/p')
+
+    # Modify the /etc/fstab file for the root partition
+    sudo sed -i -E "s|UUID=.*\s+/\s+btrfs.*|UUID=${ROOT_UUID} /               btrfs   defaults,noatime,space_cache=v2,compress=lzo,subvol=@ 0       1|" "/etc/fstab"
+
+    # Modify the /etc/fstab file for the home partition
+    sudo sed -i -E "s|UUID=.*\s+/home\s+btrfs.*|UUID=${HOME_UUID} /home           btrfs   defaults,noatime,space_cache=v2,compress=lzo,subvol=@home 0       2|" "/etc/fstab"
+
+    # Clear the screen
+    clear
+    cat /etc/fstab
+    sudo cp /etc/fstab /etc/fstab_old
+
+    # Defragment the Btrfs file system
+    sudo btrfs filesystem defragment / -r -clzo
+
+    # Mount the Btrfs device
+    root_partition=$(df -h / | awk 'NR==2 {print $1}')
+    echo "Root is mounted on partition: $root_partition"
+
+    # Create the /mnt directory if it doesn't exist
+    sudo mkdir -p /mnt
+
+    # Mount the root partition to /mnt
+    sudo mount $root_partition /mnt
+
+    # Create additional subvolumes
+    sudo btrfs subvolume create /mnt/@log
+    sudo btrfs subvolume create /mnt/@cache
+    sudo btrfs subvolume create /mnt/@tmp
+
+    # Move existing contents of /var/cache and /var/log to the new subvolumes
+    sudo mv /var/cache/* /mnt/@cache/
+    sudo mv /var/log/* /mnt/@log/
+
+    # Balance to duplicate metadata and system
+    sudo btrfs balance start -m /mnt
+
+    # Balance to set data and global reserve as non-duplicated
+    sudo btrfs balance start -d -s /mnt
+
+    # Check if the fstab file exists
+    fstab="/etc/fstab"
+    if [ -e "$fstab" ]; then
+        # Adjust compression in /etc/fstab with the new subvolumes
+        {
+            echo "# Adding New Subvolumes"
+            echo "UUID=$ROOT_UUID /var/log btrfs defaults,noatime,space_cache=v2,compress=lzo,subvol=@log 0 2"
+            echo "UUID=$ROOT_UUID /var/cache btrfs defaults,noatime,space_cache=v2,compress=lzo,subvol=@cache 0 2"
+            echo "UUID=$ROOT_UUID /var/tmp btrfs defaults,noatime,space_cache=v2,compress=lzo,subvol=@tmp 0 2"
+        } | sudo tee -a "$fstab" > /dev/null
+    else
+        echo "The file $fstab does not exist. Verify the file path."
+    fi
+
+    # Unmount the Btrfs device
+    sudo umount /mnt
+
+    # Set permissions for /var/tmp, /var/cache, and /var/log
+    sudo chmod 1777 /var/tmp/
+    sudo chmod 1777 /var/cache/
+    sudo chmod 1777 /var/log/
+
+    # Install Timeshift
+    sudo apt install timeshift build-essential git -y
+
+    # Clone the grub-btrfs repository and compile and install
+    sudo git clone https://github.com/Antynea/grub-btrfs.git /git/grub-btrfs/
+    (
+        cd /git/grub-btrfs || exit
+        sudo make install
+    )
+
+    # Install inotify-tools
+    sudo apt install inotify-tools -y
+
+    # Modify the service file to add --timeshift-auto
+    SERVICE_FILE="/lib/systemd/system/grub-btrfsd.service"
+    sudo sed -i 's|^ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' "$SERVICE_FILE"
+
+    # Reload systemd configuration
+    sudo systemctl daemon-reload
+
+    # Rename the timeshift-gtk file in /usr/bin/
+    sudo mv /usr/bin/timeshift-gtk /usr/bin/timeshift-gtk-back
+
+    # Create a new timeshift-gtk file with the given content
+    echo -e '#!/bin/bash\n/bin/pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY /usr/bin/timeshift-gtk-back' | sudo tee /usr/bin/timeshift-gtk > /dev/null
+
+    # Grant execute permissions to the new file
+    sudo chmod +x /usr/bin/timeshift-gtk
+
+    sudo timeshift --create --comments "initial"
+
+    sudo systemctl stop timeshift && sudo systemctl disable timeshift
+    sudo chmod +s /usr/bin/grub-btrfsd
+
+    # Restart the service
+    sudo systemctl restart grub-btrfsd
+    sudo systemctl start grub-btrfsd
+    sudo systemctl enable grub-btrfsd
+
+    # Update grub
+    sudo update-grub
+
+else
+    echo "The root partition is not mounted on a BTRFS volume."
+fi
+
+
+############################################
+# PIPEWIRE AND WIREPLUMBER INSTALLATION
+############################################
+
+# Get the codename of the latest LTS version of Ubuntu
+ubuntu_lts=$(curl -s https://changelogs.ubuntu.com/meta-release-lts | grep Dist: | tail -n1 | awk -F '[: ]+' '{print $NF}' | tr '[:upper:]' '[:lower:]')
+
+# Create the pipewire-upstream.list file
+echo "deb http://ppa.launchpad.net/pipewire-debian/pipewire-upstream/ubuntu $ubuntu_lts main" | sudo tee /etc/apt/sources.list.d/pipewire-upstream.list > /dev/null
+
+# Create the wireplumber-upstream.list file
+echo "deb http://ppa.launchpad.net/pipewire-debian/wireplumber-upstream/ubuntu $ubuntu_lts main" | sudo tee /etc/apt/sources.list.d/wireplumber-upstream.list > /dev/null
+
+# Install the key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 25088A0359807596
+
+# Update the system
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+
+# Install necessary packages
+sudo apt install -y \
+    libfdk-aac2 \
+    libldacbt-{abr,enc}2 \
+    libopenaptx0 \
+    gstreamer1.0-pipewire \
+    libpipewire-0.3-{0,dev,modules} \
+    libspa-0.2-{bluetooth,dev,jack,modules} \
+    pipewire{,-{audio-client-libraries,pulse,bin,locales,tests}} \
+    pipewire-doc \
+    libpipewire-module-x11-bell \
+    wireplumber{,-doc} \
+    gir1.2-wp-0.4 \
+    libwireplumber-0.4-{0,dev} \
+    wireplumber-locales
+
+# Disable PulseAudio
+systemctl --user --now disable pulseaudio.{socket,service}
+systemctl --user mask pulseaudio
+
+# Enable Pipewire
+sudo apt reinstall pipewire pipewire-bin pipewire-pulse -y
+systemctl --user --now enable pipewire pipewire-pulse
+systemctl --user --now enable pipewire{,-pulse}.{socket,service}
+systemctl --user --now enable wireplumber.service
+
+############################################
+# CLEANING AND FINAL SYSTEM UPDATE
+############################################
+
+# Clean and final system update
+sudo apt install -y bleachbit
+sudo bleachbit -c apt.autoclean apt.autoremove apt.clean system.tmp system.trash system.cache system.localizations system.desktop_entry
+sudo apt update
+sudo apt --fix-broken install
+sudo aptitude safe-upgrade -y
+
+# Reboot the system
+sudo reboot
