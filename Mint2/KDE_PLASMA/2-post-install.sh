31b631763ae348fe9a597ec21c2daa9ec664c40c
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2024-01-22 19:00:39 +0100

    Fix

--- Mint2/KDE_PLASMA/2-post-install.sh
+++ Mint2/KDE_PLASMA/2-post-install.sh
@@ -18,48 +18,28 @@ echo "KONSOLE & DOTFILES"
 sleep 3
 
 dir="$(pwd)"
-sudo git clone https://github.com/fastfetch-cli/fastfetch.git /tmp/fastfetch/
-cd /tmp/fastfetch/
+sudo git clone https://github.com/fastfetch-cli/fastfetch.git /git/linux-config/SparkyLinux/KDE_PLASMA/fastfetch/
+cd /git/linux-config/SparkyLinux/KDE_PLASMA/fastfetch/
 sudo mkdir -p build
 cd build
 sudo cmake ..
 sudo cmake --build . --target fastfetch --target flashfetch
 sudo cp fastfetch flashfetch /usr/bin/
 cd ${dir}
-mkdir ~/.config/fastfetch/
 fastfetch --gen-config-force
-cp -r dotfiles/fastfetch_config.jsonc ~/.config/fastfetch/config.jsonc
+cp -r ../dotfiles/fastfetch_config.jsonc ~/.config/fastfetch/config.jsonc
+cp -r ../dotfiles/sparky ~/.config/fastfetch/sparky
 
 sudo wget https://github.com/gabrielomana/color_schemes/raw/main/konsole.zip
 sudo unzip konsole.zip
 sudo cp konsole/* /usr/share/konsole/ -rf
 sudo rm konsole/ -rf
 
-cp -r dotfiles/neofetch.conf ~/.config/neofetch/config.conf
-cp -r dotfiles/topgrade.toml ~/.config/topgrade.toml
-cp -r dotfiles/.nanorc ~/.config/.nanorc
-cp -r dotfiles/konsole.profile ~/.local/share/konsole/konsole.profile
-cp -r dotfiles/konsolerc ~/.config/konsolerc
-
-# Descargar y configurar esquemas de colores para Konsole
-sudo wget -q https://github.com/gabrielomana/color_schemes/raw/main/konsole.zip -P /tmp
-sudo unzip -q /tmp/konsole.zip -d /tmp
-sudo cp -r /tmp/konsole/* /usr/share/konsole/
-sudo rm -rf /tmp/konsole/
-
-# Configurar perfil de Konsole
-cp -r dotfiles/konsole.profile ~/.local/share/konsole/konsole.profile
-
-# Confiqurar featherpad
-featherpad &
-sleep 3
-killall featherpad
-sleep 1
-cp -r dotfiles/fp.conf ~/.config/featherpad/fp.conf
-
-# Configurar neofetch y topgrade
-cp -r dotfiles/neofetch.conf ~/.config/neofetch/config.conf
-cp -r dotfiles/topgrade.toml ~/.config/topgrade.toml
+cp -r ../dotfiles/neofetch.conf ~/.config/neofetch/config.conf
+cp -r ../dotfiles/topgrade.toml ~/.config/topgrade.toml
+cp -r ../dotfiles/.nanorc ~/.config/.nanorc
+cp -r ../dotfiles/konsole.profile ~/.local/share/konsole/konsole.profile
+cp -r ../dotfiles/konsolerc ~/.config/konsolerc
 
 ########## EXTRA APPS #############################################
 clear
