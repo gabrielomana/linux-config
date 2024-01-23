eba792bef2c57bfc66ab71e8dbe3bd452b7f6793
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2024-01-23 17:11:05 +0100

    Fix

--- OpenSUSE/script.sh
+++ OpenSUSE/script.sh
@@ -17,8 +17,6 @@ update_repo="http://download.opensuse.org/update/tumbleweed/"
 src_oss_repo="http://download.opensuse.org/tumbleweed/repo/src-oss"
 src_non_oss_repo="http://download.opensuse.org/tumbleweed/repo/src-non-oss"
 debug_oss_repo="http://download.opensuse.org/debug/tumbleweed/repo/oss/"
-backports_repo="http://download.opensuse.org/update/tumbleweed/backports/"
-backports_debug_repo="http://download.opensuse.org/update/tumbleweed/backports_debug/"
 
 # Repositorios Packman adicionales
 packman_complete_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/"
@@ -27,8 +25,6 @@ packman_multimedia_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUS
 packman_games_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Games/"
 packman_extra_repo="https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Extra/"
 libdvdcss_repo="https://opensuse-guide.org/repo/openSUSE_Tumbleweed/"
-nvidia_repo="https://download.nvidia.com/opensuse/tumbleweed/"
-google_repo="https://www.google.com/linuxrepositories/"
 
 # Repositorios adicionales de fuentes y juegos
 m17n_fonts_repo="http://download.opensuse.org/repositories/M17N:/fonts/openSUSE_Tumbleweed/"
@@ -41,8 +37,6 @@ zypper ar -cfp 99 $update_repo update
 zypper ar -cfp 99 $src_oss_repo src-oss
 zypper ar -cfp 99 $src_non_oss_repo src-non-oss
 zypper ar -cfp 99 $debug_oss_repo debug-oss
-zypper ar -cfp 95 $backports_repo backports
-zypper ar -cfp 95 $backports_debug_repo backports-debug
 
 # Repositorios Packman
 zypper ar -cfp 90 $packman_complete_repo packman-complete
@@ -51,8 +45,6 @@ zypper ar -cfp 90 $packman_multimedia_repo packman-multimedia
 zypper ar -cfp 90 $packman_games_repo packman-games
 zypper ar -cfp 90 $packman_extra_repo packman-extra
 zypper ar -cfp 90 $libdvdcss_repo libdvdcss
-zypper ar -cfp 90 $nvidia_repo nvidia
-zypper ar -cfp 90 $google_repo google
 
 # Repositorios adicionales
 zypper ar -cfp 90 $m17n_fonts_repo m17n-cfp 90onts
@@ -94,30 +86,7 @@ sudo flatpak remote-modify --system --prio=4 fedora
 echo "+-----------------------------------------------------+"
 echo "| Descarga e instalación de Codecs KDE para openSUSE  |"
 echo "+-----------------------------------------------------+"
-
-
-# URL del archivo YMP
-ymp_url="https://www.opensuse-community.org/codecs-kde.ymp"
-
-# Directorio temporal para descargar el archivo
-temp_dir=$(mktemp -d)
-
-# Descargar el archivo YMP
-wget "$ymp_url" -P "$temp_dir"
-
-# Verificar si la descarga fue exitosa
-if [ $? -eq 0 ]; then
-  echo "Descarga exitosa. Iniciando la instalación."
-
-  # Ejecutar YAST con el archivo YMP
-  sudo yast2 sw_single "$temp_dir/codecs-kde.ymp"
-
-  # Limpiar el directorio temporal
-  rm -r "$temp_dir"
-else
-  echo "Error al descargar el archivo. Verifica la URL e inténtalo nuevamente."
-fi
-sudo zypper -n install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2 --auto-agree-with-licenses
+sudo zypper -n install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2
 sudo zypper -n install opi
 sudo opi codecs
 
