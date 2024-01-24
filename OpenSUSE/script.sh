df01fbe5c278b324c979b86bc44f53dcc3d72380
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2024-01-24 17:50:34 +0100

    Fix

--- OpenSUSE/script.sh
+++ OpenSUSE/script.sh
@@ -6,6 +6,13 @@ if [ "$UID" -ne 0 ]; then
 fi
 
 echo "+-----------------------------------------------------+"
+echo "| SNAPPER                                             |"
+echo "+-----------------------------------------------------+"
+sudo zypper ar -cfp 95 https://download.opensuse.org/repositories/home:michael-chang:grub:2.12rc1/openSUSE_Tumbleweed/home:michael-chang:grub:2.12rc1.repo
+sudo zypper --gpg-auto-import-keys ref
+sudo zypper install -n grub2-snapper-plugin
+
+echo "+-----------------------------------------------------+"
 echo "| Refresh                                              |"
 echo "+-----------------------------------------------------+"
     sudo zypper -n refresh
@@ -123,31 +130,6 @@ sudo cp ../dotfiles/fonts.conf /etc/fonts/fonts.conf -rf
 # Actualizar la caché de fuentes
 sudo fc-cache -f -v
 
-# echo "+-----------------------------------------------------+"
-# echo "| Configuración de Zswap completada en openSUSE       |"
-# echo "+-----------------------------------------------------+"
-#     # Directorio temporal para realizar copias de seguridad
-#     backup_dir="/tmp/zswap_backup"
-#     mkdir -p "$backup_dir"
-
-#     # Copia de seguridad del archivo sysctl.conf
-#     sudo cp /etc/sysctl.conf "$backup_dir/sysctl.conf_backup"
-
-#     # Añadir configuración de Zswap a sysctl.conf
-#     echo "vm.swappiness=25" | sudo tee -a /etc/sysctl.conf
-#     echo "vm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf
-#     echo "vm.zswap.enabled=1" | sudo tee -a /etc/sysctl.conf
-#     echo "vm.zswap.zpool=z3fold" | sudo tee -a /etc/sysctl.conf
-#     echo "vm.zswap.compressor=lz4" | sudo tee -a /etc/sysctl.conf
-
-#     # Actualizar la configuración del kernel
-#     sudo sysctl -p
-
-#     # Actualizar GRUB
-#     sudo cp /etc/default/grub /etc/default/grub_backup
-#     echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet zswap.enabled=1 zswap.zpool=z3fold zswap.compressor=lz4"' | sudo tee -a /etc/default/grub
-#     sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-
 
 # Cambio de nombre del host
 echo "+------------------------------------------------------------------+"
@@ -165,3 +147,95 @@ echo "+-----------------------------------------------------+"
     sudo chmod +x /usr/bin/rpm
 
 
+echo "+-----------------------------------------------------+"
+echo "| Microcode  & VAAPI                                  |"
+echo "+-----------------------------------------------------+"
+    # Verifica que la GPU sea compatible con la aceleración de hardware
+    gpu_info=$(lspci | grep -i "3D controller")
+    if [ -z "$gpu_info" ]; then
+    echo "La GPU no es compatible con la aceleración de hardware."
+    exit 1
+    fi
+
+    # Obtiene el nombre del fabricante de la GPU.
+    gpu_vendor=$(lspci | grep -i "3D controller" | awk '{print $3}')
+
+    # Instala los controladores de la GPU.
+    case "$gpu_vendor" in
+    Intel*)
+        echo "Instalando controladores para la GPU Intel..."
+        sudo zypper install -y intel-media-driver intel-hybrid-driver
+        ;;
+    AMD*)
+        echo "Instalando controladores para la GPU AMD..."
+        sudo zypper install -y kernel-firmware-amdgpu xf86-video-amdgpu Mesa-drivers libva-vdpau-driver
+        ;;
+    NVIDIA*)
+        echo "Instalando controladores para la GPU NVIDIA..."
+        zypper install-new-recommends --repo NVIDIA
+        # Determinar la versión del controlador según la información del hardware
+        gpu_info=$(lspci | grep -i "VGA" | grep -Ei "NVIDIA|GPU")
+        if echo "$gpu_info" | grep -qi "GeForce 600\|GeForce 700"; then
+        driver_version="G05"
+        elif echo "$gpu_info" | grep -qi "GeForce 400\|GeForce 500"; then
+        driver_version="G04"
+        else
+        driver_version="G06"
+        fi
+        # Instalar el controlador según la versión determinada
+        sudo zypper in x11-video-nvidia${driver_version} nvidia-gl${driver_version} nvidia-compute${driver_version}
+        ;;
+    esac
+
+    # Configuración del procesador
+    # Obtiene el nombre del procesador.
+    cpu_name=$(lscpu | grep -Ei "Model name" | awk '{print $3}')
+
+    # Instala los controladores y microcódigos apropiados.
+    if echo "$cpu_name" | grep -qi "intel"; then
+    echo "Instalando controladores y microcódigos para el procesador Intel..."
+    sudo zypper install -y ucode-intel iucode-tool
+    elif echo "$cpu_name" | grep -qi "amd"; then
+    echo "Instalando controladores y microcódigos para el procesador AMD..."
+    sudo zypper install -y ucode-amd
+    else
+    echo "No se pudo identificar el procesador."
+    fi
+    # Configuración de los controladores de máquinas virtuales
+    # Verifica si la máquina es un host de máquinas virtuales.
+    if [[ "$(cat /sys/class/dmi/id/chassis_type)" != "Machine" && "$(cat /sys/class/dmi/id/chassis_type)" != "Maquina" ]]; then
+    # Obtiene el nombre del hypervisor.
+    hypervisor=$(sudo dmidecode -s system-product-name)
+
+
+echo "+-----------------------------------------------------+"
+echo "| ZSWAP                                               |"
+echo "+-----------------------------------------------------+"
+    # Actualiza los módulos del kernel
+    sudo zypper ref
+    # Activa el soporte para lz4hc
+    sudo modprobe lz4hc lz4hc_compress
+    # Crea el archivo de configuración de dracut
+    sudo touch /etc/dracut.conf.d/lz4hc.conf
+    # Agrega lz4hc a la lista de módulos
+    echo "add_drivers+=\"lz4hc lz4hc_compress\"" | sudo tee -a /etc/dracut.conf.d/lz4hc.conf
+    # Regenera los archivos initramfs
+    sudo dracut --regenerate-all --force
+    # Establece el compresor de zswap a lz4hc
+    echo "lz4hc" | sudo tee /sys/module/zswap/parameters/compressor
+    # Establece el tamaño máximo del pool de memoria comprimida al 25% de la RAM
+    echo "25" | sudo tee /sys/module/zswap/parameters/max_pool_percent
+    # Activa zswap
+    echo "1" | sudo tee /sys/module/zswap/parameters/enabled
+    # Busca la línea que comienza por "GRUB_CMDLINE_LINUX"
+    linea_grub=$(grep -n "GRUB_CMDLINE_LINUX" /etc/default/grub | cut -d: -f1)
+    # Obtiene el contenido de la línea
+    contenido_grub=$(sed -n "$linea_grub"p /etc/default/grub)
+    # Agrega los argumentos de zswap al final de la línea
+    contenido_grub="$contenido_grub zswap.enabled=1 zswap.max_pool_percent=25 zswap.compressor=lz4hc"
+    # Elimina la última comilla doble
+    contenido_grub=${contenido_grub%"\""}
+    # Escribe el nuevo contenido en el archivo
+    sed -i "${linea_grub}s/.*/$contenido_grub/" /etc/default/grub
+    # Actualiza la configuración de GRUB
+    sudo grub2-mkconfig -o /boot/grub2/grub.cfg
