21a2c2986f7144bd830d58995accdb283f2b7527
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2024-02-01 12:41:08 +0100

    Fix

--- Fedora/1-pre_install.sh
+++ Fedora/1-pre_install.sh
@@ -363,6 +363,7 @@ function configure-zswap {
 function set-btrfs {
     # Verificar si la partición root está en Btrfs
     if [[ $(df -T / | awk 'NR==2 {print $2}') == "btrfs" ]]; then
+        sudo cp /etc/fstab /etc/fstab_old
         # Obtener el UUID de la partición raíz
         ROOT_UUID=$(grep -E '/\s+btrfs\s+' "/etc/fstab" | awk '{print $1}' | sed -n 's/UUID=\(.*\)/\1/p')
         # Obtener el UUID de la partición home
@@ -374,15 +375,11 @@ function set-btrfs {
         # Limpiar la pantalla
         clear
         cat /etc/fstab
-        sudo cp /etc/fstab /etc/fstab_old
         # Desfragmentar el sistema de archivos Btrfs
         sudo btrfs filesystem defragment / -r -clzo
         # Montar el dispositivo Btrfs
         root_partition=$(df -h / | awk 'NR==2 {print $1}')
         echo "La raíz está montada en la partición: $root_partition"
-        echo "#### 1 ###"
-        sleep 10
-        clear
         # Creamos el directorio /mnt si no existe
         sudo mkdir -p /mnt
         # Montamos la partición raíz en /mnt
@@ -391,22 +388,13 @@ function set-btrfs {
         sudo btrfs subvolume create /mnt/@log
         sudo btrfs subvolume create /mnt/@cache
         sudo btrfs subvolume create /mnt/@tmp
-        echo "#### 2 ###"
-        sleep 10
-        clear
         # Mover los contenidos existentes de /var/cache y /var/log a los nuevos subvolúmenes
         sudo mv /var/cache/* /mnt/@cache/
         sudo mv /var/log/* /mnt/@log/
-        echo "#### 3 ###"
-        sleep 10
-        clear
         # Balanceo para duplicar metadatos y sistema
         sudo btrfs balance start -m --force /mnt
         # Balanceo para configurar datos y reserva global como no duplicados
         sudo btrfs balance start -d -s --force /mnt
-        echo "#### 4 ###"
-        sleep 10
-        clear
         # Verificar si el archivo fstab existe
         fstab="/etc/fstab"
         if [ -e "$fstab" ]; then
@@ -420,69 +408,52 @@ function set-btrfs {
         else
             echo "El archivo $fstab no existe. Verifica la ruta del archivo."
         fi
-        echo "#### 5 ###"
-        sleep 10
-        clear
         # Desmontar el dispositivo Btrfs
         sudo umount /mnt --force
-        echo "#### 6 ###"
-        sleep 10
-        clear
 
         # Establecer permisos para /var/tmp, /var/cache y /var/log
         sudo chmod 1777 /var/tmp/
         sudo chmod 1777 /var/cache/
         sudo chmod 1777 /var/log/
-        echo "#### 7 ###"
-        sleep 10
-        clear
-        # Instalar Timeshift
-        sudo dnf install timeshift -y  
-        # Instalar el repositorio de grub-btrfs
-        sudo dnf copr enable kylegospo/grub-btrfs -y
-        sudo dnf update -y
-        sudo dnf install -y grub-btrfs grub-btrfs-timeshift inotify-tools
-        echo "#### 8 ###"
-        sleep 10
-        clear
 
-        # Modificar el archivo del servicio para agregar --timeshift-auto
-        SERVICE_FILE="/lib/systemd/system/grub-btrfsd.service"
-        sudo sed -i 's|^ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' "$SERVICE_FILE"
+        # Install Timeshift
+    sudo dnf install -y timeshift inotify-tools
 
-        # Recargar la configuración de systemd
-        sudo systemctl daemon-reload
-        echo "#### 9 ###"
-        sleep 10
-        clear
-        # Cambia el nombre del archivo timeshift-gtk en /usr/bin/
-        sudo mv /usr/bin/timeshift-gtk /usr/bin/timeshift-gtk-back
+    # Clone the grub-btrfs repository and compile and install
+    sudo git clone https://github.com/Antynea/grub-btrfs.git /git/grub-btrfs/
+    (
+        cd /git/grub-btrfs
+        sed -i '/#GRUB_BTRFS_SNAPSHOT_KERNEL/a GRUB_BTRFS_SNAPSHOT_KERNEL_PARAMETERS="systemd.volatile=state"' config
+        sed -i '/#GRUB_BTRFS_GRUB_DIRNAME/a GRUB_BTRFS_GRUB_DIRNAME="/boot/grub2"' config
+        sed -i '/#GRUB_BTRFS_MKCONFIG=/a GRUB_BTRFS_MKCONFIG=/sbin/grub2-mkconfig' config
+        sed -i '/#GRUB_BTRFS_SCRIPT_CHECK=/a GRUB_BTRFS_SCRIPT_CHECK=grub2-script-check' config
+        sudo make install
+    )
 
-        # Crea un nuevo archivo timeshift-gtk con el contenido dado
-        echo -e '#!/bin/bash\n/bin/pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY /usr/bin/timeshift-gtk-back' | sudo tee /usr/bin/timeshift-gtk > /dev/null
+    # Modify the service file to add --timeshift-auto
+    SERVICE_FILE="/lib/systemd/system/grub-btrfsd.service"
+    sudo sed -i 's|^ExecStart=/usr/bin/grub-btrfsd --syslog /.snapshots|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' "$SERVICE_FILE"
 
-        # Otorga permisos de ejecución al nuevo archivo
-        sudo chmod +x /usr/bin/timeshift-gtk
-        sudo timeshift --create --comments "initial"
-        echo "#### 10 ###"
-        sleep 10
-        clear
-        sudo systemctl stop timeshift && sudo systemctl disable timeshift
-        sudo chmod +s /usr/bin/grub-btrfsd
-        
-        # Reiniciar el servicio
-        sudo systemctl restart grub-btrfs.path
-        sudo systemctl start grub-btrfs.path
-        sudo systemctl enable --now grub-btrfs.path
-        echo "#### 11 ###"
-        sleep 10
-        clear
+    # Reload systemd configuration
+    sudo systemctl daemon-reload
 
-        # Actualizar grub
-        sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-        echo "#### 12 ###"
-        sleep 10
-        clear
+    # Rename the timeshift-gtk file in /usr/bin/
+    sudo mv /usr/bin/timeshift-gtk /usr/bin/timeshift-gtk-back
+
+    # Create a new timeshift-gtk file with the given content
+    echo -e '#!/bin/bash\n/bin/pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY /usr/bin/timeshift-gtk-back' | sudo tee /usr/bin/timeshift-gtk > /dev/null
+
+    # Grant execute permissions to the new file
+    sudo chmod +x /usr/bin/timeshift-gtk
+
+    sudo timeshift --create --comments "initial"
+
+    sudo systemctl stop timeshift && sudo systemctl disable timeshift
+    sudo chmod +s /usr/bin/grub-btrfsd
+
+    # Restart the service
+    sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+    sudo systemctl enable --now grub-btrfsd.service
 
     else
         echo "La partición root no está montada en un volumen BTRFS."
