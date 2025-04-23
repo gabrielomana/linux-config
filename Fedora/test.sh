0323ebccaf1d374630d6a436037adccdc20e991b
gabrielomana <36016056+gabrielomana@users.noreply.github.com>
2025-04-24 00:28:04 +0200

    Fix

--- Fedora/test.sh
+++ Fedora/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-cat > /etc/systemd/system/grub-btrfs@.path <<'EOF'
+    sudo tee /etc/systemd/system/grub-btrfs@.path > /dev/null <<'EOF'
 [Unit]
 Description=Monitors Timeshift snapshots in /run/timeshift/%i/backup
 DefaultDependencies=no
@@ -12,3 +11,8 @@ PathModified=/run/timeshift/%i/backup/timeshift-btrfs/snapshots
 [Install]
 WantedBy=run-timeshift-%i.mount
 EOF
+
+    sudo systemctl daemon-reexec
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now grub-btrfs@backup.path || error "Fallo al activar grub-btrfs@backup.path"
+
